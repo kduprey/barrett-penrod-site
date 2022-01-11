fc108a455293b2ae08f7bcbb01c9c687948ed8bb
Kenton Duprey <kentonduprey@icloud.com>
2022-01-11 18:45:01 +0000

    changing flex position

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,13 @@ import Header from "./components/Header";
 
 const Home = () => {
 	return (
-		<div className="flex flex-col items-center min-h-screen py-2">
+		<div className="flex flex-col items-center justify-center min-h-screen py-2">
 			<Header
 				title="Barrett Penrod - Actor & Voice Teacher"
 				description="Site is under construction"
 			/>
 
-			<main className="container">
+			<main>
 				<h1 className="text-center text-4xl">
 					Site is under construction
 				</h1>

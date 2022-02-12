17ccff390ce49ae41ecffaade6baa6516fb31946
Kenton Duprey <kentonduprey@icloud.com>
2022-02-12 22:17:05 +0000

    updating dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,29 @@
-const colors = require("tailwindcss/colors");
+// const colors = require("tailwindcss/colors");
 
-module.exports = {
-	content: [
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		colors: {
-			transparent: "transparent",
-			current: "currentColor",
-			primary: colors.violet,
-			secondary: colors.teal,
-			black: colors.black,
-			white: colors.white,
-			gray: colors.slate,
-			danger: colors.red,
-			warning: colors.amber,
-			success: colors.emerald,
-		},
-		fontFamily: {
-			sans: ["Inter", "sans-serif"],
-			serif: ["Georgia", "serif"],
-			mono: ["Menlo", "Monaco", "Consolas", "monospace"],
-		},
-		extend: {},
-	},
-	plugins: [require("@tailwindcss/forms")],
-};
+// module.exports = {
+// 	content: [
+// 		"./pages/**/*.{js,ts,jsx,tsx}",
+// 		"./components/**/*.{js,ts,jsx,tsx}",
+// 	],
+// 	theme: {
+// 		colors: {
+// 			transparent: "transparent",
+// 			current: "currentColor",
+// 			primary: colors.violet,
+// 			secondary: colors.teal,
+// 			black: colors.black,
+// 			white: colors.white,
+// 			gray: colors.slate,
+// 			danger: colors.red,
+// 			warning: colors.amber,
+// 			success: colors.emerald,
+// 		},
+// 		fontFamily: {
+// 			sans: ["Inter", "sans-serif"],
+// 			serif: ["Georgia", "serif"],
+// 			mono: ["Menlo", "Monaco", "Consolas", "monospace"],
+// 		},
+// 		extend: {},
+// 	},
+// 	plugins: [require("@tailwindcss/forms")],
+// };

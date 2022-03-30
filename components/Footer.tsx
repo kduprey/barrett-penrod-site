43bf809ac811a5450fee0251505ee89ea4a7fb49
Kenton Duprey <kentonduprey@icloud.com>
2022-03-30 14:49:42 +0100

    refactoring footer into component

---
+++ components/Footer.tsx
@@ -0,0 +1,49 @@
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+type Props = {
+	facebook: IconDefinition;
+	twitter: IconDefinition;
+	instagram: IconDefinition;
+	youtube: IconDefinition;
+};
+
+const Footer = (props: Props) => {
+	return (
+		<div className="bg-primary flex flex-col justify-center items-center space-y-4 py-6">
+			<h1 className="text-secondary">Contact</h1>
+
+			<h2 className="text-white font-normal text-xl underline underline-offset-2">
+				Representation
+			</h2>
+
+			<div className="flex flex-col justify-center items-center space-y-4 text-white">
+				<h3>Manager</h3>
+				<p>Bruce Robertson</p>
+				<p>Email - info@site.com</p>
+				<p>Tel - (123)456-7890</p>
+			</div>
+			<div className="flex flex-col justify-center items-center space-y-4 text-white">
+				<h3>Commercial Agent</h3>
+				<p>Magnum - Steve Macfee</p>
+				<p>Email - info@site.com</p>
+				<p>Tel - (123)456-7890</p>
+			</div>
+			<div className="flex flex-col justify-center items-center space-y-4 text-white">
+				<h3>SF Agent</h3>
+				<p>Pinnacle - Nathan Kelly</p>
+				<p>Email - info@site.com</p>
+				<p>Tel - (123)456-7890</p>
+			</div>
+			{/* Social Media Links */}
+			<div className="flex justify-evenly items-center text-white w-4/5 pt-4">
+				<FontAwesomeIcon icon={props.facebook} />
+				<FontAwesomeIcon icon={props.twitter} />
+				<FontAwesomeIcon icon={props.instagram} />
+				<FontAwesomeIcon icon={props.youtube} />
+			</div>
+		</div>
+	);
+};
+
+export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,13 @@ import Header from "../components/Header";
 import { NavMenu } from "../types";
 import MenuButton from "../components/Navbar/MenuButton";
 import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
 	faFacebookF,
 	faTwitter,
 	faInstagram,
 	faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
+import Footer from "../components/Footer";
 
 const Home: NextPage = () => {
 	const [isOpen, setIsOpen] = useState(false);
@@ -188,39 +188,12 @@ const Home: NextPage = () => {
 
 			{/* Contact */}
 
-			<div className="bg-primary flex flex-col justify-center items-center space-y-4 py-6">
-				<h1 className="text-secondary">Contact</h1>
-
-				<h2 className="text-white font-normal text-xl underline underline-offset-2">
-					Representation
-				</h2>
-
-				<div className="flex flex-col justify-center items-center space-y-4 text-white">
-					<h3>Manager</h3>
-					<p>Bruce Robertson</p>
-					<p>Email - info@site.com</p>
-					<p>Tel - (123)456-7890</p>
-				</div>
-				<div className="flex flex-col justify-center items-center space-y-4 text-white">
-					<h3>Commercial Agent</h3>
-					<p>Magnum - Steve Macfee</p>
-					<p>Email - info@site.com</p>
-					<p>Tel - (123)456-7890</p>
-				</div>
-				<div className="flex flex-col justify-center items-center space-y-4 text-white">
-					<h3>SF Agent</h3>
-					<p>Pinnacle - Nathan Kelly</p>
-					<p>Email - info@site.com</p>
-					<p>Tel - (123)456-7890</p>
-				</div>
-				{/* Social Media Links */}
-				<div className="flex justify-evenly items-center text-white w-4/5 pt-4">
-					<FontAwesomeIcon icon={faFacebookF} />
-					<FontAwesomeIcon icon={faTwitter} />
-					<FontAwesomeIcon icon={faInstagram} />
-					<FontAwesomeIcon icon={faYoutube} />
-				</div>
-			</div>
+			<Footer
+				facebook={faFacebookF}
+				twitter={faTwitter}
+				instagram={faInstagram}
+				youtube={faYoutube}
+			/>
 		</main>
 	);
 };

c9d14070acb94dc0725cfa3cf5a8eaaa5dfc96c7
Kenton Duprey <kentonduprey@icloud.com>
2022-04-13 00:27:14 +0100

    adding link to name on titlebar

--- components/Navbar/TitleBar.tsx
+++ components/Navbar/TitleBar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import MenuButton from "../../components/MenuButton";
 
 type Props = {
@@ -8,9 +9,11 @@ type Props = {
 const TitleBar = ({ isNavOpen, setIsNavOpen }: Props) => {
 	return (
 		<div className="sticky top-0 z-50 flex items-center justify-between bg-primary px-8 md:pt-6">
-			<h2 className="text-xl font-light text-secondary sm:text-2xl md:font-medium">
-				Barrett Penrod
-			</h2>
+			<Link href={"/"} passHref>
+				<h2 className="cursor-pointer text-xl font-light text-secondary hover:text-white sm:text-2xl md:font-medium">
+					Barrett Penrod
+				</h2>
+			</Link>
 			<MenuButton isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
 		</div>
 	);

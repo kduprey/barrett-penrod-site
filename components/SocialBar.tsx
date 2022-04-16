00c0eef0a5ca7fa54ac28e6b9dafde803e7623be
Kenton Duprey <kentonduprey@icloud.com>
2022-04-16 16:14:00 +0100

    fixing social links on social bar

--- components/SocialBar.tsx
+++ components/SocialBar.tsx
@@ -13,16 +13,32 @@ const SocialBar = (props: Props) => {
 	return (
 		<>
 			<div className="mx-6 flex items-center justify-evenly bg-secondary p-2 text-black md:hidden">
-				<FontAwesomeIcon icon={faFacebookF} />
-				<FontAwesomeIcon icon={faTwitter} />
-				<FontAwesomeIcon icon={faInstagram} />
-				<FontAwesomeIcon icon={faYoutube} />
+				<a
+					className="text-black"
+					href="https://twitter.com/penrodbarrett?s=21&t=cXVPSIuXbwvd6pTGNYZz2A"
+				>
+					<FontAwesomeIcon icon={faTwitter} />
+				</a>
+				<a
+					className="text-black"
+					href="https://instagram.com/barrettpenrod?igshid=YmMyMTA2M2Y="
+				>
+					<FontAwesomeIcon icon={faInstagram} />
+				</a>
 			</div>
 			<div className="fixed top-1/2 right-0 z-20 hidden h-40 w-12 flex-col justify-evenly bg-secondary md:flex">
-				<FontAwesomeIcon icon={faFacebookF} />
-				<FontAwesomeIcon icon={faTwitter} />
-				<FontAwesomeIcon icon={faInstagram} />
-				<FontAwesomeIcon icon={faYoutube} />
+				<a
+					className="pl-4 text-black"
+					href="https://twitter.com/penrodbarrett?s=21&t=cXVPSIuXbwvd6pTGNYZz2A"
+				>
+					<FontAwesomeIcon icon={faTwitter} />
+				</a>
+				<a
+					className="pl-4 text-black"
+					href="https://instagram.com/barrettpenrod?igshid=YmMyMTA2M2Y="
+				>
+					<FontAwesomeIcon icon={faInstagram} />
+				</a>
 			</div>
 		</>
 	);

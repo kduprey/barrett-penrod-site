ad26e137386871d339dd83abe63696fdacea0580
Kenton Duprey <kentonduprey@icloud.com>
2022-04-25 23:41:09 +0100

    adding redirect for recital

--- components/News.tsx
+++ components/News.tsx
@@ -9,38 +9,6 @@ const News = (props: Props) => {
 			<h1 className="text-center text-secondary">News</h1>
 
 			<div className="p-6 text-white">
-				<div className="flex flex-col items-center justify-center space-y-3 bg-[#57068c] p-4">
-					<h3 className="font-normal">
-						In Performance - April 18, 2022
-					</h3>
-					<div>
-						<div>NYU Class of 2022 Professional Showcase</div>
-						<div className="flex space-x-2">
-							<div>Where -</div>
-							<a
-								className="text-white underline underline-offset-2 hover:text-slate-300"
-								href="https://goo.gl/maps/9VZCULgcTspi2KFq8"
-							>
-								New World Stages <br />
-								340 W 50th Street,
-								<br />
-								New York, NY <br />
-								10019
-							</a>
-						</div>
-						<div>When - 3:30pm & 6:30pm EST</div>
-						<div>
-							RSVP - Please contact Jen Bender at{" "}
-							<a
-								className="text-secondary underline underline-offset-2"
-								href="mailto:jnb9579@nyu.edu"
-							>
-								jnb9579@nyu.edu
-							</a>
-						</div>
-					</div>
-				</div>
-
 				<div className="flex flex-col items-center justify-center p-4">
 					<h3 className="font-normal text-white">
 						In Performance - April 30, 2022

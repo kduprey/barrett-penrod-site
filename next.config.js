817dd8b02a25f8b977a269a75558a5df9186b578
Kenton Duprey <kentonduprey@icloud.com>
2022-05-05 10:04:27 -0400

    adding redirect back with recital link

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	async redirects() {
+		return [
+			{
+				source: "/recital",
+				destination: "https://youtu.be/-atRUPhiGxU",
+				permanent: true,
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;

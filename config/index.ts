a3102d4a7bcbfc1857d32fc73a929db0a5fb1409
Kenton Duprey <kentonduprey@icloud.com>
2022-07-12 19:26:08 -0400

    adding server config

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,7 @@
 const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
-	? "https://1185-108-27-247-65.ngrok.io"
+	? "https://24c0-216-1-111-253.ngrok.io"
 	: "https://barrettpenrod.com";
+
+export const db = dev ? "test" : "prod";

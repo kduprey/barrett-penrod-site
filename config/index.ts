cfec5c33d42fcbd821c3b3bc65536fd7dd59a149
Kenton Duprey <kentonduprey@icloud.com>
2022-07-26 16:57:23 +0100

    adding cookie types

--- config/index.ts
+++ config/index.ts
@@ -3,16 +3,14 @@ import Stripe from "stripe";
 const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
-	? "https://d3c5-108-27-247-65.ngrok.io"
+	? "https://fda3-185-236-186-94.eu.ngrok.io/"
 	: "https://barrettpenrod.com";
 
 export const db = dev ? "test" : "production";
 
 export const stripeMode = dev ? "test" : "live";
 
-export const dbURL = dev
-	? "mongodb://localhost:27017"
-	: process.env.PRODUCTION_DB_URL;
+export const dbURL = process.env.PRODUCTION_DB_URL;
 
 export const stripe = new Stripe(
 	process.env.NODE_ENV === "production"

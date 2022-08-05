3f701f65c0be95702de945804af95499b346dfcc
Kenton Duprey <kentonduprey@icloud.com>
2022-08-05 00:17:12 -0400

    adding calendly endpoint

---
+++ pages/api/webhooks/calendly.ts
@@ -0,0 +1,113 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import crypto from "crypto";
+import { CalendlyEventInvitee, InviteeResource } from "../../../types";
+import axios from "axios";
+
+type Data = {};
+
+const calendlyWebhook = async (
+	req: NextApiRequest,
+	res: NextApiResponse<Data>
+) => {
+	const webhookSigningKey = process.env
+		.CALENDLY_WEBHOOK_SIGNING_KEY as string;
+
+	// Extract the timestamp and signature from the header
+
+	const calendlySignature = req.headers[
+		"Calendly-Webhook-Signature"
+	] as string;
+
+	const { t, signature } = calendlySignature?.split(",").reduce(
+		(acc, currentValue) => {
+			const [key, value] = currentValue.split("=");
+			if (key === "t") {
+				// UNIX timestamp
+				acc.t = value;
+			}
+			if (key === "v1") {
+				acc.signature = value;
+			}
+			return acc;
+		},
+		{
+			t: "",
+			signature: "",
+		}
+	);
+
+	if (!t || !signature) throw new Error("Invalid Signature");
+
+	// Create the signed payload by concatenating the timestamp (t), the character '.' and the request body's JSON payload.
+
+	const data = t + "." + JSON.stringify(req.body);
+
+	const expectedSignature = crypto
+		.createHmac("sha256", webhookSigningKey)
+		.update(data, "utf8")
+		.digest("hex");
+
+	// Determine the expected signature by computing an HMAC with the SHA256 hash function.
+
+	if (expectedSignature !== signature) {
+		// Signature is invalid!
+
+		throw new Error("Invalid Signature");
+	}
+
+	const threeMinutes = 180000;
+	const tolerance = threeMinutes;
+	const timestampMilliseconds = Number(t) * 1000;
+
+	if (timestampMilliseconds < Date.now() - tolerance) {
+		// Signature is invalid!
+		// The signature's timestamp is outside of the tolerance zone defined above.
+		throw new Error(
+			"Invalid Signature. The signature's timestamp is outside of the tolerance zone."
+		);
+	}
+
+	// Signature is valid!
+
+	if (req.body.event === "invitee.created") {
+		const invitee: InviteeResource = req.body.data.payload;
+		const eventInvitee: CalendlyEventInvitee = {
+			resource: invitee,
+		};
+
+		const response = await axios.post(
+			"/api/db/eventInvitees",
+			eventInvitee
+		);
+
+		if (response.status === 200) {
+			return res.status(200).json(response.data);
+		}
+		return res.status(500).json({
+			err: new Error("Error: Failed to insert client"),
+			response,
+		});
+	}
+
+	if (req.body.event === "invitee.canceled") {
+		const invitee: InviteeResource = req.body.data.payload;
+		const eventInvitee: CalendlyEventInvitee = {
+			resource: invitee,
+		};
+
+		const response = await axios.post(
+			"/api/db/eventInvitees",
+			eventInvitee
+		);
+
+		if (response.status === 200) {
+			return res.status(200).json(response.data);
+		}
+		return res.status(500).json({
+			err: new Error("Error: Failed to insert client"),
+			response,
+		});
+	}
+};
+
+export default calendlyWebhook;

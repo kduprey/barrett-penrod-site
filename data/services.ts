901a006221484bd5d0ef8fc4632cdbbb22c297ed
Kenton Duprey <kentonduprey@icloud.com>
2022-08-05 00:13:54 -0400

    updating services

--- data/services.ts
+++ data/services.ts
@@ -1,4 +1,4 @@
-import { LessonBundle } from "../types";
+import { LessonBundle, Price } from "../types";
 
 type ServiceType = {
 	title: string;
@@ -94,7 +94,7 @@ export const services: ServiceType[] = [
 		title: "SVS Session",
 		description:
 			"One hour and fifteen minute habilitative session focused towards the singing voice for singers who are currently/formerly undergoing vocal rehabilitative treatment with a Speech Language Pathologist and Laryngologist.",
-		locations: ["Open Jar", "Virtual", "Location Chosen By Client"],
+		locations: ["Location Chosen By Client", "Open Jar", "Virtual"],
 		get url() {
 			const title = this.title;
 			return this.locations.map((location) => {
@@ -116,15 +116,19 @@ export const services: ServiceType[] = [
 	},
 ];
 
-export const packages: LessonPackage[] = [
+export const bundleServices: ServiceType[] = services.filter((e) => {
+	return e.title !== "SVS Session";
+});
+
+export const bundles: LessonBundle[] = [
 	{
 		amtSessions: 4,
 		title: "4 Session Package",
 		discount: "1/2 off a session",
 		price: "280",
-		stripeID: {
-			test: "price_1LLKKfAMm0G1mbCtsNQnB6jW",
-			live: "price_1LLKHdAMm0G1mbCtVZ9UmBPv",
+		priceID: {
+			test: { price: "price_1LLKKfAMm0G1mbCtsNQnB6jW", quantity: 1 },
+			live: { price: "price_1LLKHdAMm0G1mbCtVZ9UmBPv", quantity: 1 },
 		},
 	},
 	{
@@ -132,9 +136,9 @@ export const packages: LessonPackage[] = [
 		title: "6 Session Package",
 		discount: "1 session free",
 		price: "400",
-		stripeID: {
-			test: "price_1LLKKtAMm0G1mbCtRGAQOxp7",
-			live: "price_1LLKIlAMm0G1mbCtcZ3PDgqq",
+		priceID: {
+			test: { price: "price_1LLKKtAMm0G1mbCtRGAQOxp7", quantity: 1 },
+			live: { price: "price_1LLKIlAMm0G1mbCtcZ3PDgqq", quantity: 1 },
 		},
 	},
 	{
@@ -142,9 +146,9 @@ export const packages: LessonPackage[] = [
 		title: "8 Session Package",
 		discount: "1 and 1/2 sessions free",
 		price: "520",
-		stripeID: {
-			test: "price_1LLKLAAMm0G1mbCt2zHaZ8BU",
-			live: "price_1LLKJAAMm0G1mbCtcijAzVhU",
+		priceID: {
+			test: { price: "price_1LLKLAAMm0G1mbCt2zHaZ8BU", quantity: 1 },
+			live: { price: "price_1LLKJAAMm0G1mbCtcijAzVhU", quantity: 1 },
 		},
 	},
 	{
@@ -152,9 +156,33 @@ export const packages: LessonPackage[] = [
 		title: "12 Session Package",
 		discount: "2 sessions free",
 		price: "800",
-		stripeID: {
-			test: "price_1LLKLLAMm0G1mbCtE0ubYKM2",
-			live: "price_1LLKJMAMm0G1mbCtSUld32rj",
+		priceID: {
+			test: { price: "price_1LLKLLAMm0G1mbCtE0ubYKM2", quantity: 1 },
+			live: { price: "price_1LLKJMAMm0G1mbCtSUld32rj", quantity: 1 },
+		},
+	},
+];
+
+export const Prices: Price[] = [
+	{
+		name: "SVS Session",
+		priceID: {
+			test: { price: "price_1LDhdeAMm0G1mbCt3nyv52uI", quantity: 1 },
+			live: { price: "price_1LDhS8AMm0G1mbCtVVMPsJiS", quantity: 1 },
+		},
+	},
+	{
+		name: "Regular Session",
+		priceID: {
+			test: { price: "price_1LDhdeAMm0G1mbCtGFA3BJEJ", quantity: 1 },
+			live: { price: "price_1L88w7AMm0G1mbCt5xei9unc", quantity: 1 },
+		},
+	},
+	{
+		name: "Open Jar Booking Fee",
+		priceID: {
+			test: { price: "price_1LPn53AMm0G1mbCt5MtVjvox", quantity: 1 },
+			live: { price: "price_1LPn4aAMm0G1mbCtwDPP0yWq", quantity: 1 },
 		},
 	},
 ];

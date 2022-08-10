0fd35cd39d59cde8f759e665c916769406494e60
Kenton Duprey <kentonduprey@icloud.com>
2022-08-10 00:53:40 -0400

    adjusted the border of the logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,7 +2,7 @@ import VsLogo from "/public/vslogo.svg";
 
 const Logo = () => {
 	return (
-		<div className="logo w-3/4 p-3 md:w-1/4">
+		<div className="logo w-3/4 p-8 md:w-1/4">
 			<VsLogo />
 		</div>
 	);

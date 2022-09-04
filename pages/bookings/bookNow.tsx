ac790f60feaf73de789908a1e9a3c3b8799b0db8
Kenton Duprey <kentonduprey@icloud.com>
2022-09-03 20:15:39 -0400

    removing fill and chaning path tag

--- pages/bookings/bookNow.tsx
+++ pages/bookings/bookNow.tsx
@@ -90,21 +90,17 @@ const Page: NextPageWithLayout = () => {
 					xmlns="http://www.w3.org/2000/svg"
 					width="64"
 					height="64"
-					fill="none"
+					fill="currentColor"
 					viewBox="0 0 24 24"
 					className="animate-spin"
 				>
 					<path
-						fill="currentColor"
 						fillRule="evenodd"
 						d="M12 19a7 7 0 100-14 7 7 0 000 14zm0 3c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"
 						clipRule="evenodd"
 						opacity="0.2"
 					></path>
-					<path
-						fill="currentColor"
-						d="M2 12C2 6.477 6.477 2 12 2v3a7 7 0 00-7 7H2z"
-					></path>
+					<path d="M2 12C2 6.477 6.477 2 12 2v3a7 7 0 00-7 7H2z"></path>
 				</svg>
 			</div>
 		);

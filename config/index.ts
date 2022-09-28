596a6705b7ebd73608941daccae5dc6c6a1581a3
Kenton Duprey <kentonduprey@icloud.com>
2022-09-27 23:33:33 -0400

    refactoring config file env var

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,6 @@
-import { MailService } from "@sendgrid/mail";
 import Stripe from "stripe";
 
-const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.VERCEL_ENV !== "production";
 
 export const server = dev
 	? "https://3fb9-2601-180-c200-c710-e1ff-7424-83ff-1b1e.ngrok.io"
@@ -14,7 +13,7 @@ export const stripeMode = dev ? "test" : "live";
 export const dbURL = process.env["PRODUCTION_DB_URL"];
 
 export const stripe = new Stripe(
-	process.env.NODE_ENV === "production"
+	process.env.VERCEL_ENV === "production"
 		? `${process.env["STRIPE_SECRET_KEY"]}`
 		: `${process.env["STRIPE_TEST_SECRET_KEY"]}`,
 	{
@@ -22,14 +21,10 @@ export const stripe = new Stripe(
 	}
 );
 
-const sendgridClient: MailService = require("@sendgrid/mail");
-if (process.env.NODE_ENV === "production") {
-	console.log("PROD:", process.env["NEXT_PUBLIC_SENDGRID_API_KEY"]);
-	sendgridClient.setApiKey(
-		process.env["NEXT_PUBLIC_SENDGRID_API_KEY"] as string
-	);
+import sendgridClient from "@sendgrid/mail";
+if (process.env.VERCEL_ENV === "production") {
+	sendgridClient.setApiKey(process.env["SENDGRID_API_KEY"] as string);
 } else {
-	console.log("DEV:", process.env["NEXT_PUBLIC_SENDGRID_DEV_API_KEY"]);
 	sendgridClient.setApiKey(process.env["SENDGRID_DEV_API_KEY"] as string);
 }
 export const sendgrid = sendgridClient;

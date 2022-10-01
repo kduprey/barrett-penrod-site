035f3f7acb4c836b2284265f80f071ef2ffaecef
Kenton Duprey <kentonduprey@icloud.com>
2022-10-01 17:55:00 -0400

    adding more structure to cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,47 @@
 import { defineConfig } from "cypress";
 
+const execa = require("execa");
+const findBrowser = () => {
+	// the path is hard-coded for simplicity
+	const browserPath =
+		"/Applications/Brave Browser.app/Contents/MacOS/Brave Browser";
+
+	return execa(browserPath, ["--version"]).then(
+		(result: { stdout: string }) => {
+			// STDOUT will be like "Brave Browser 77.0.69.135"
+			const [, version] = /Brave Browser (\d+\.\d+\.\d+\.\d+)/.exec(
+				result.stdout
+			);
+			const majorVersion = parseInt(version.split(".")[0]);
+
+			return {
+				name: "Brave",
+				channel: "stable",
+				family: "chromium",
+				displayName: "Brave",
+				version,
+				path: browserPath,
+				majorVersion,
+			};
+		}
+	);
+};
+
 export default defineConfig({
 	projectId: "kzgfbk",
+	fixturesFolder: false,
 	e2e: {
 		setupNodeEvents(on, config) {
-			// implement node event listeners here
+			return findBrowser().then(
+				(browser: ConcatArray<Cypress.Browser>) => {
+					return {
+						browsers: config.browsers.concat(browser),
+					};
+				}
+			);
 		},
+		baseUrl: "http://localhost:3000",
+		specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+		supportFile: "cypress/support/e2e.{js,jsx,ts,tsx}",
 	},
 });

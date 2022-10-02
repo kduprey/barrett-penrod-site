f89dd25d821e1532b4cb7a50d360e1f2f9a0e4d1
Kenton Duprey <kentonduprey@icloud.com>
2022-10-01 20:35:06 -0400

    adding remaining tests for home page

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -59,4 +59,104 @@ describe("Homepage", () => {
 			});
 		});
 	});
+
+	context("Resume Section", () => {
+		it("should have a resume section", () => {
+			cy.getByData("resume").should("exist");
+		});
+
+		it("should download when clicked", () => {
+			cy.document().then((doc) => {
+				const downloadURL = doc
+					.querySelector("[data-cy=download-resume]")
+					.getAttribute("href");
+
+				cy.request(downloadURL).then((response) => {
+					expect(response.status).to.eq(200);
+				});
+			});
+		});
+	});
+
+	context("Showreel Section", () => {
+		it("should have a showreel section", () => {
+			cy.getByData("showreel").should("exist");
+		});
+	});
+
+	context("Production Photos", () => {
+		it("should have a production photos section", () => {
+			cy.getByData("production-photos").should("exist");
+		});
+	});
+
+	context("News Section", () => {
+		it("should have a news section", () => {
+			cy.getByData("news").should("exist");
+		});
+	});
+
+	context("Contact Section", () => {
+		it("should have a contact section", () => {
+			cy.getByData("contact").should("exist");
+		});
+
+		it("should have a contact form", () => {
+			cy.getByData("contact-form").should("exist");
+		});
+
+		it("should have a contact form name field", () => {
+			cy.getByData("contact-form-name").should("exist");
+		});
+
+		it("should have a contact form email field", () => {
+			cy.getByData("contact-form-email").should("exist");
+		});
+
+		it("should have a contact form message field", () => {
+			cy.getByData("contact-form-message").should("exist");
+		});
+
+		it("should have a contact form submit button", () => {
+			cy.getByData("contact-form-submit").should("exist");
+		});
+
+		it("should submit the form when the submit button is clicked", () => {
+			cy.intercept("POST", "/api/contact", {
+				statusCode: 200,
+				body: {
+					id: "123",
+				},
+			}).as("contactFormSubmit");
+
+			cy.getByData("contact-form-name").type("Barrett Penrod");
+			cy.getByData("contact-form-email").type("test@email.com");
+			cy.getByData("contact-form-message").type("This is a test message");
+			cy.getByData("contact-form-submit").click();
+			cy.getByData("success-message").should("exist");
+		});
+
+		it("should show an error message when the form fails to submit", () => {
+			cy.intercept("POST", "/api/contact", {
+				statusCode: 500,
+			}).as("contactFormSubmit");
+
+			cy.getByData("contact-form-name").type("Barrett Penrod");
+			cy.getByData("contact-form-email").type("test@email.com");
+			cy.getByData("contact-form-message").type("This is a test message");
+			cy.getByData("contact-form-submit").click();
+			cy.getByData("error-message").should("exist");
+		});
+
+		it("should show error if form is not filled out", () => {
+			cy.intercept("POST", "/api/contact", {
+				statusCode: 500,
+			}).as("contactFormSubmit");
+
+			cy.getByData("contact-form-name").type("Barrett Penrod");
+			cy.getByData("contact-form-message").type("This is a test message");
+			cy.getByData("contact-form-submit").click();
+			cy.getByData("error-message").should("exist");
+		});
+	});
 });

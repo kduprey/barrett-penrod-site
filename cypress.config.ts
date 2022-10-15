991b18395cdf31a50e0d5faedfdff64146b91cb9
Kenton Duprey <kentonduprey@icloud.com>
2022-10-15 15:36:13 -0400

    removing cypress browser support for Brave

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,39 +2,15 @@ import { defineConfig } from "cypress";
 import fs from "fs";
 import path from "path";
 
-const execa = require("execa");
-const findBrowser = () => {
-	// the path is hard-coded for simplicity
-	const browserPath =
-		"/Applications/Brave Browser.app/Contents/MacOS/Brave Browser";
-
-	return execa(browserPath, ["--version"]).then(
-		(result: { stdout: string }) => {
-			// STDOUT will be like "Brave Browser 77.0.69.135"
-			// @ts-ignore
-			const [, version] = /Brave Browser (\d+\.\d+\.\d+\.\d+)/.exec(
-				result.stdout
-			);
-			const majorVersion = parseInt(version.split(".")[0]);
-
-			return {
-				name: "Brave",
-				channel: "stable",
-				family: "chromium",
-				displayName: "Brave",
-				version,
-				path: browserPath,
-				majorVersion,
-			};
-		}
-	);
-};
-
 export default defineConfig({
 	projectId: "kzgfbk",
 	fixturesFolder: false,
 	chromeWebSecurity: false,
+
 	e2e: {
+		baseUrl: "http://localhost:3000",
+		specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+		supportFile: "cypress/support/e2e.{js,jsx,ts,tsx}",
 		setupNodeEvents(on, config) {
 			on("task", {
 				findFile(fileName) {
@@ -49,16 +25,7 @@ export default defineConfig({
 				},
 			});
 
-			return findBrowser().then(
-				(browser: ConcatArray<Cypress.Browser>) => {
-					return {
-						browsers: config.browsers.concat(browser),
-					};
-				}
-			);
+			return config;
 		},
-		baseUrl: "http://localhost:3000",
-		specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
-		supportFile: "cypress/support/e2e.{js,jsx,ts,tsx}",
 	},
 });

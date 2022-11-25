9d1f2d7d644fd1b174ca365af9f39f1ad80c5b96
Kenton Duprey <kentonduprey@icloud.com>
2022-11-25 18:49:49 -0500

    Updating config with prisma defaults and exporting dev boolean

--- config/index.ts
+++ config/index.ts
@@ -1,9 +1,11 @@
+import { PrismaClientOptions } from "@prisma/client/runtime";
+import sendgridClient from "@sendgrid/mail";
 import Stripe from "stripe";
 
-const dev = process.env.VERCEL_ENV !== "production";
+export const dev = process.env.VERCEL_ENV !== "production";
 
 export const server = dev
-	? "http://localhost:3000"
+	? "https://3d8e-24-103-41-10.ngrok.io"
 	: "https://barrettpenrod.com";
 
 export const db = dev ? "test" : "production";
@@ -25,10 +27,29 @@ export const stripeWebhookSecret = dev
 	? process.env["STRIPE_TEST_WEBHOOK_SECRET"]
 	: process.env["STRIPE_WEBHOOK_SECRET"];
 
-import sendgridClient from "@sendgrid/mail";
 if (process.env.VERCEL_ENV === "production") {
 	sendgridClient.setApiKey(process.env["SENDGRID_API_KEY"] as string);
 } else {
 	sendgridClient.setApiKey(process.env["SENDGRID_DEV_API_KEY"] as string);
 }
 export const sendgrid = sendgridClient;
+
+export const prismaConfig = dev
+	? {
+			log: [
+				"query",
+				"info",
+				"warn",
+				"error",
+			] as PrismaClientOptions["log"],
+			errorFormat: "pretty" as PrismaClientOptions["errorFormat"],
+	  }
+	: {
+			log: [
+				"query",
+				"info",
+				"warn",
+				"error",
+			] as PrismaClientOptions["log"],
+			errorFormat: "minimal" as PrismaClientOptions["errorFormat"],
+	  };

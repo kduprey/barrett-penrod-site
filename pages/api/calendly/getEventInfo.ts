054779583b2dfc7c7bcc882f70cd56ed362a550a
Kenton Duprey <kentonduprey@icloud.com>
2022-11-25 18:48:54 -0500

    refactoring getEventInfo endpoint

---
+++ pages/api/calendly/getEventInfo.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosResponse } from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { CalendlyEvent } from "../../../types/types";
+import { invalidMethod } from "../../../utils/responseDefaults";
+
+const getEventInfo = async (
+	uri: string
+): Promise<AxiosResponse<CalendlyEvent>> => {
+	return await axios.get<CalendlyEvent>(uri, {
+		headers: {
+			Authorization: `Bearer ${process.env["CALENDLY_API_KEY"]}`,
+		},
+	});
+};
+export { getEventInfo };
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+	const { uri } = req.body as { uri: string };
+	if (!uri || typeof uri !== "string") {
+		res.status(400).json({ message: "Missing uri" });
+		return;
+	}
+	invalidMethod("POST", req, res);
+
+	try {
+		const eventRes = await getEventInfo(uri);
+		res.status(200).json(eventRes.data);
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ message: "Error getting event" });
+	}
+};
+
+export default handler;

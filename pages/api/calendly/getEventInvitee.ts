72d61288970c760a75989946262e4845b74b3d1e
Kenton Duprey <kentonduprey@icloud.com>
2022-11-27 14:07:35 -0500

    Updating getEventInvitee to extract method and added more error handling

--- pages/api/calendly/getEventInvitee.ts
+++ pages/api/calendly/getEventInvitee.ts
@@ -1,27 +1,34 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
-import { CalendlyInviteePayload } from "../../../types";
-
-type Data = {};
+import { CalendlyInvitee } from "../../../types/types";
+import { invalidMethod } from "../../../utils/responseDefaults";
 
 const getEventInvitee = async (
-	req: NextApiRequest,
-	res: NextApiResponse<CalendlyInviteePayload | Data>
-) => {
-	const { uri } = req.body as { uri: string };
-
-	const inviteeRes = await axios.get(uri, {
+	uri: string
+): Promise<AxiosResponse<CalendlyInvitee>> => {
+	return await axios.get<CalendlyInvitee>(uri, {
 		headers: {
 			Authorization: `Bearer ${process.env["CALENDLY_API_KEY"]}`,
 		},
 	});
-
-	if (inviteeRes.status !== 200) {
-		res.status(500).json(inviteeRes);
-	}
-
-	const invitee: CalendlyInviteePayload = inviteeRes.data;
-	res.status(200).json(invitee);
 };
 
-export default getEventInvitee;
+export { getEventInvitee };
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+	const { uri } = req.body as { uri: string };
+	if (!uri || typeof uri !== "string")
+		res.status(400).json({ message: "Missing uri" });
+
+	invalidMethod("POST", req, res);
+
+	try {
+		const inviteeRes = await getEventInvitee(uri);
+		res.status(200).json(inviteeRes.data);
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ message: "Error getting invitee" });
+	}
+};
+
+export default handler;

cc6d1ac10baad5a150b05cef16f9626d7d36aed2
Kenton Duprey <kentonduprey@icloud.com>
2022-12-31 20:40:13 -0500

    Adjusting BookingNavbar for responsiveness

--- components/Navbar/BookingNavbar.tsx
+++ components/Navbar/BookingNavbar.tsx
@@ -8,71 +8,59 @@ const BookingNavbar = (props: Props) => {
 	const [isNavOpen, setIsNavOpen] = useState(false);
 
 	return (
-		<>
-			<div className="sticky top-0 z-[60] flex items-center justify-between bg-primary px-8 py-4 shadow-lg md:flex-row md:justify-between md:pt-6">
-				<Link href={"/bookings"} passHref>
-					<h1 className="cursor-pointer text-xl font-light text-secondary hover:text-white sm:text-2xl md:font-medium">
-						Barrett Penrod <br className="md:hidden" /> Voice Studio
-					</h1>
+		<nav className="booking sticky top-0 left-0 z-[60] flex max-h-[6em] w-full items-center justify-between bg-primary p-5 shadow-lg md:justify-between md:pt-6">
+			<Link href={"/bookings"} passHref>
+				<h1 className="max-w-[9em] text-lg font-light text-secondary hover:text-white sm:text-xl sm:font-medium lg:text-2xl">
+					<span>Barrett Penrod </span>
+					<span>Voice Studio</span>
+				</h1>
+			</Link>
+
+			<MenuButton isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
+
+			<div className="hidden items-center justify-evenly text-center md:flex md:w-full">
+				<Link href={"/bookings/consultation"}>
+					<p>Free Consultation</p>
 				</Link>
-
-				<MenuButton isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
-
-				<div className=" hidden w-full items-center justify-evenly md:flex md:w-auto md:grow">
-					<Link href={"/bookings/consultation"}>
-						<p className="withTransition cursor-pointer opacity-90 hover:text-white hover:underline hover:opacity-100">
-							Free Consultation
-						</p>
-					</Link>
-					<Link href={"/bookings/individual"}>
-						<p className="withTransition cursor-pointer opacity-90 hover:text-white hover:underline hover:opacity-100">
-							Individual Bookings
-						</p>
-					</Link>
-					<Link href={"/bookings/bundles"}>
-						<p className="withTransition cursor-pointer opacity-90 hover:text-white hover:underline hover:opacity-100">
-							Lesson Packages
-						</p>
-					</Link>
-				</div>
-				<Link href={"/"} passHref>
-					<p className="withTransition hidden cursor-pointer opacity-90  hover:text-white hover:underline hover:opacity-100 md:block">
-						Back to Acting Portfolio
-					</p>
+				<Link href={"/bookings/individual"}>
+					<p>Individual Bookings</p>
+				</Link>
+				<Link href={"/bookings/bundles"}>
+					<p>Lesson Packages</p>
 				</Link>
 			</div>
+			<Link href={"/"} passHref>
+				<p className="hidden max-w-[9em] whitespace-pre text-right md:block">
+					Back to <br /> Acting Portfolio
+				</p>
+			</Link>
+			{/* Mobile Nav Modal */}
 			<div
-				className={`fixed top-0 z-20 flex flex-col items-center bg-primary p-6 transition-all duration-300 ease-in-out md:hidden  ${
+				className={`fixed top-[6em] z-[70] flex flex-col items-center bg-primary p-6 transition-all duration-300 ease-in-out md:hidden ${
 					isNavOpen
 						? "left-0 h-full w-full pt-44"
 						: "-left-full h-0 w-0"
 				}`}
 				onClick={() => setIsNavOpen(false)}
 			>
-				<div className="flex flex-col items-center justify-center space-y-3">
+				<div className="flex flex-col items-center justify-center space-y-5">
 					<Link href={"/bookings/consultation"}>
-						<p className="withTransition cursor-pointer text-lg opacity-90 hover:text-white hover:underline hover:opacity-100">
-							Free Consultation
-						</p>
+						<p className="link text-2xl">Free Consultation</p>
 					</Link>
 					<Link href={"/bookings/individual"}>
-						<p className="withTransition cursor-pointer text-lg opacity-90 hover:text-white hover:underline hover:opacity-100">
-							Individual Bookings
-						</p>
+						<p className="link text-2xl">Individual Bookings</p>
 					</Link>
 					<Link href={"/bookings/bundles"}>
-						<p className="withTransition cursor-pointer text-lg opacity-90 hover:text-white hover:underline hover:opacity-100">
-							Lesson Packages
-						</p>
+						<p className="link text-2xl">Lesson Packages</p>
 					</Link>
 				</div>
 				<Link href={"/"} passHref>
-					<p className="withTransition cursor-pointer pt-20 opacity-90  hover:text-white hover:underline hover:opacity-100">
+					<p className="link pt-20 text-2xl">
 						Back to Acting Portfolio
 					</p>
 				</Link>
 			</div>
-		</>
+		</nav>
 	);
 };
 

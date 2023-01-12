39620bf7cbbad460f6f31f74a9b13025fe522f1c
Kenton Duprey <kentonduprey@icloud.com>
2023-01-11 21:04:55 -0500

    Refactoring Contact api endpoint into individual method for testing and to maintain consistency

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,4 +1,7 @@
+import axios, { AxiosResponse } from "axios";
 import { NextApiRequest, NextApiResponse } from "next/types";
+import { AirTableResponse } from "types/airtableTypes";
+import { invalidMethod } from "utils/responseDefaults";
 
 const API_KEY = process.env["AIRTABLE_API_KEY"];
 
@@ -6,47 +9,15 @@ export type ContactFormBody = {
 	name: string;
 	email: string;
 	message: string;
-	age: number;
+	age?: number;
 };
 
-const contact = async (req: NextApiRequest, res: NextApiResponse) => {
-	const { name, email, message, age } = req.body as ContactFormBody;
-	const base = `appgICv9x4N8ACmHB`;
-	const table = `Contact%20Form`;
-
-	if (!name || !email || !message) {
-		return res.status(400).json({
-			error: "Missing required fields",
-		});
-	}
-
-	if (
-		name === "Test" &&
-		email === "email@test.com" &&
-		message === "This is a test message"
-	) {
-		const response = await fetch(
-			`https://api.airtable.com/v0/${base}/${table}?fields%5B%5D=Name&maxRecords=2&returnFieldsByFieldId=true`,
-			{
-				method: "GET",
-				headers: {
-					Authorization: `Bearer ${API_KEY}`,
-				},
-			}
-		);
-
-		if (response.ok) {
-			const data = await response.json();
-
-			return res.status(200).json(data);
-		} else {
-			return res.status(500).json({
-				error: "Something went wrong",
-				message: response.body,
-			});
-		}
-	}
-
+const contact = async ({
+	name,
+	email,
+	message,
+	age,
+}: ContactFormBody): Promise<AirTableResponse | Error> => {
 	const data = {
 		Name: name,
 		Email: email,
@@ -54,35 +25,52 @@ const contact = async (req: NextApiRequest, res: NextApiResponse) => {
 	};
 
 	if (age) {
+		return new Error("Not a valid field");
+	}
+	try {
+		const response = await axios<AirTableResponse>(
+			process.env["AIRTABLE_API_URL"] as string,
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${API_KEY}`,
+				},
+				data: {
+					records: [
+						{
+							fields: data,
+						},
+					],
+				},
+			}
+		);
+
+		return response.data;
+	} catch (error) {
+		console.log(error);
+	}
+
+	return new Error("Error sending message");
+};
+
+export { contact };
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+	invalidMethod("POST", req, res);
+
+	if (!req.body)
 		res.status(400).json({
 			status: 400,
 			message: "Bad Request",
 		});
-		return;
-	}
 
-	fetch(`https://api.airtable.com/v0/${base}/${table}`, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			Authorization: `Bearer ${API_KEY}`,
-		},
-		body: JSON.stringify({ fields: data }),
-	})
-		.then((response) => response.json())
-		.then((data) => {
-			if (data.error) {
-				res.status(500).json({
-					status: 500,
-					message: data.error,
-				});
-				return;
-			}
-			res.status(200).json(data);
-		})
-		.catch((err) => {
-			res.status(500).json(err);
-		});
+	try {
+		const data = contact(req.body);
+		res.status(200).json(data);
+	} catch (error) {
+		res.status(500).json(error);
+	}
 };
 
-export default contact;
+export default handler;

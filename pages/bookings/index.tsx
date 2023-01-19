ba5dbd0d1701b7ac07aab3c1bb851beb69311a84
Kenton Duprey <kentonduprey@icloud.com>
2023-01-18 19:20:58 -0500

    Extracting sections from bookings/index

--- pages/bookings/index.tsx
+++ pages/bookings/index.tsx
@@ -1,9 +1,16 @@
+import AboutStudio from "components/Bookings/AboutStudio";
+import Bio from "components/Bookings/Bio";
+import ConsultationSession from "components/Bookings/ConsultationSession";
+import IndividualSession from "components/Bookings/IndividualSession";
+import LessonDemo from "components/Bookings/LessonDemo";
+import Services from "components/Bookings/Services";
 import { gql } from "graphql-request";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import BundleModal from "../../components/Bookings/BundleModal";
+import Bundles from "../../components/Bookings/Bundles";
 import BookingsLayout from "../../components/BookingsLayout";
 import AboutStudioSection from "../../components/content/AboutStudioSection";
 import DemosSection from "../../components/content/DemosSection";
@@ -11,7 +18,7 @@ import MoreAboutSection from "../../components/content/MoreAboutSection";
 import Logo from "../../components/Logo";
 import SidePanel from "../../components/Modals/SidePanel";
 import { hygraphcms } from "../../config";
-import { bundles, services } from "../../data/services";
+import { services } from "../../data/services";
 import headshot2 from "../../public/headshot2.jpg";
 import { NextPageWithLayout } from "../../types/types";
 import handleQueryParams from "../../utils/handleQueryParams";
@@ -49,34 +56,10 @@ const Bookings: NextPageWithLayout = ({ promoContents }: Props) => {
 		handleQueryParams(router.query);
 	}, [router.query]);
 
-	const [isBundleModalOpen, setIsBundleModalOpen] = useState(false);
-	const [isDemosOpen, setIsDemosOpen] = useState(false);
-	const [isMoreAboutOpen, setIsMoreAboutOpen] = useState(false);
-	const [selectedBundle, setselectedBundle] = useState<number>();
-
 	return (
 		<section
 			className={` flex flex-col items-center justify-center gap-3 px-6`}
 		>
-			<BundleModal
-				isOpen={isBundleModalOpen}
-				setIsOpen={setIsBundleModalOpen}
-				selectedBundle={selectedBundle}
-			/>
-			<SidePanel
-				isOpen={isDemosOpen}
-				setIsOpen={setIsDemosOpen}
-				header="Demos"
-			>
-				<DemosSection />
-			</SidePanel>
-			<SidePanel
-				isOpen={isMoreAboutOpen}
-				setIsOpen={setIsMoreAboutOpen}
-				header="More About Me"
-			>
-				<MoreAboutSection />
-			</SidePanel>
 			{/* Promo Section */}
 			{promoContents[0] && (
 				<div className="flex flex-col items-center justify-center gap-3 p-3">
@@ -92,167 +75,31 @@ const Bookings: NextPageWithLayout = ({ promoContents }: Props) => {
 			</div>
 
 			{/* Bio & Philiosophy */}
-			<div className=" flex w-full max-w-screen-lg flex-col items-center justify-center  md:flex-row md:items-start md:justify-evenly md:gap-5 lg:max-w-screen-xl ">
-				{/* Headshot */}
-				<div className=" max-w-[20em]">
-					<Image
-						loading="lazy"
-						placeholder="blur"
-						src={headshot2}
-						alt="Barret Penrod"
-						className="rounded-xl"
-					/>
-				</div>
-				<div className="flex w-full max-w-md flex-col gap-5 lg:max-w-xl">
-					<h2>Welcome!</h2>
-					<p>
-						Thank you for visiting my studio page. I began the
-						Barrett Penrod Voice Studio officially in June 2022,
-						after completing my dual graduate degrees at New York
-						University. I&rsquo;ll be up front with you - I&rsquo;m
-						not like most other voice teachers. I&rsquo;m not going
-						to mold you into a packaged perfect sound, I&rsquo;m not
-						going to push you towards a particular method, and
-						I&rsquo;m not ever at any point going to tell you that
-						you sound bad. What I am going to do is give you the
-						tools to become truly skilled, no matter what your
-						ability is at the start. If you&rsquo;re a pro and happy
-						with your sound, then we&rsquo;re going to keep your
-						voice how you need it to be. If you wanna change, then
-						we&rsquo;ll create the way you want to be heard. If
-						you&rsquo;re a beginner, then you&rsquo;ll learn the
-						tools that will open doors you never thought possible.
-						Book in with me, and let&rsquo;s hear you do wonders!
-					</p>
-				</div>
-			</div>
+			<Bio />
 
 			<hr className="my-3 w-full rounded bg-secondary p-1" />
 
 			{/* About The Studio */}
-			<section className="">
-				<AboutStudioSection />
-				<div className="flex w-full justify-center pt-6">
-					<button onClick={() => setIsMoreAboutOpen(true)}>
-						Learn More
-					</button>
-				</div>
-			</section>
+			<AboutStudio />
 
 			<hr className="my-3 w-full rounded bg-secondary p-1" />
 			{/* Lesson Demo */}
-			<section>
-				<div className="hidden md:block">
-					<DemosSection />
-				</div>
-				<div className="flex flex-col items-center gap-3 py-3 md:hidden">
-					<h3 className="text-center">Want to see a lesson?</h3>
-					<button onClick={() => setIsDemosOpen(true)}>
-						Watch Now!
-					</button>
-				</div>
-			</section>
+			<LessonDemo />
+
 			<hr className="my-3 w-full rounded bg-secondary p-1" />
 
 			{/* Services */}
 			<section className="flex flex-wrap items-center justify-center gap-5 2xl:h-full 2xl:w-full 2xl:justify-evenly">
-				<div>
-					<h2 className="pb-6 text-center text-secondary">
-						Services
-					</h2>
-					<div className="grid justify-center gap-6 sm:grid-cols-2">
-						{services.map((service) => {
-							return (
-								<div
-									key={service.title}
-									className="withTransition flex max-w-sm origin-center transform flex-col items-center rounded-lg bg-secondary p-6 shadow-md hover:scale-105 hover:opacity-90"
-								>
-									<h3 className="text-center text-lg leading-6 text-primary md:text-2xl md:tracking-tight">
-										{service.title}
-									</h3>
-									<p className="my-auto text-center text-primary md:text-lg">
-										{service.description}
-									</p>
-								</div>
-							);
-						})}
-					</div>
-				</div>
+				<Services />
 
 				{/* Consultation Section */}
-				<div className=" flex h-full max-w-md flex-col gap-3 rounded-lg bg-secondary p-8 md:max-w-lg lg:max-w-xl  2xl:justify-between ">
-					{/* Info Section */}
-					<h2 className="text-center text-primary md:text-3xl lg:text-4xl">
-						Book a 20 min <br /> FREE consultation
-					</h2>
-					<p className="text-justify text-primary ">
-						A FREE 20 minute zoom meeting where you will meet me,
-						discuss your goals, wants, and needs for your voice
-						journey, and learn more about me, my methodology and
-						teaching style. At the end of the consultation we will
-						either book you for your first session, or you will
-						leave with a list contact info for several of my
-						colleagues who may be a closer match! Book your free
-						consultation here today!
-					</p>
-					<button className="mx-auto bg-primary">
-						<Link
-							href="/bookings/consultation"
-							className="text-secondary"
-						>
-							Book Now
-						</Link>
-					</button>
-				</div>
+				<ConsultationSession />
 			</section>
 
 			{/* Discount Packages */}
-			<section>
-				<h2 className="pb-6 text-center text-secondary md:text-3xl lg:text-4xl xl:pt-5">
-					Discount Packages
-				</h2>
-				<div className="grid gap-6 sm:grid-cols-2">
-					{bundles.map((bundle, index) => {
-						return (
-							<div
-								key={bundle.title}
-								className="withTransition flex max-w-sm flex-col items-center justify-center space-y-3 rounded-lg bg-secondary p-6 shadow-md hover:scale-105 hover:opacity-90"
-								onClick={(e) => {
-									setIsBundleModalOpen(true);
-									setselectedBundle(index);
-								}}
-							>
-								<h4 className="text-center text-primary">
-									{bundle.title}
-								</h4>
-								<p className="text-center text-primary">
-									{bundle.discount}
-								</p>
-								<p className="text-center text-3xl font-bold text-primary">
-									${bundle.price}
-								</p>
-								<button
-									className="cursor-pointer bg-primary text-secondary"
-									onClick={(e) => {
-										setIsBundleModalOpen(true);
-										setselectedBundle(index);
-									}}
-								>
-									Reserve Now!
-								</button>
-							</div>
-						);
-					})}
-				</div>
-			</section>
-			<div className="flex flex-col items-center justify-center py-6">
-				<h2 className="pb-3 text-center text-secondary md:text-3xl lg:text-4xl">
-					Book an <br /> Individual Session!
-				</h2>
-				<Link href="/bookings/individual">
-					<button>Reserve Now!</button>
-				</Link>
-			</div>
+			<Bundles />
+
+			<IndividualSession />
 		</section>
 	);
 };

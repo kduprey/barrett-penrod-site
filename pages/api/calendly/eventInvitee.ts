c0bf5ad97521a0664822805f465771da977c3c5a
Kenton Duprey <kentonduprey@icloud.com>
2023-01-18 21:48:17 -0500

    Updating eventInvitee naming, and error handling

---
+++ pages/api/calendly/eventInvitee.ts
@@ -0,0 +1,56 @@
+import axios, { AxiosResponse } from "axios";
+import createHttpError from "http-errors";
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import apiHandler from "utils/api";
+import { CalendlyInvitee } from "../../../types/types";
+import { invalidMethod } from "../../../utils/responseDefaults";
+
+const getEventInvitee = async (uri: string): Promise<CalendlyInvitee> => {
+	if (uri === undefined || uri === "") throw new Error("Invalid URI");
+	try {
+		const { data } = await axios.get<CalendlyInvitee>(uri, {
+			headers: {
+				Authorization: `Bearer ${process.env["CALENDLY_API_KEY"]}`,
+			},
+		});
+
+		return data;
+	} catch (err) {
+		console.error(err);
+		throw new Error("Error getting invitee");
+	}
+};
+
+export { getEventInvitee };
+
+const eventInviteeHandler: NextApiHandler = async (
+	req: NextApiRequest,
+	res: NextApiResponse
+) => {
+	const { uri } = req.query;
+	if (
+		!uri ||
+		typeof uri !== "string" ||
+		uri.length === 0 ||
+		uri === "undefined" ||
+		uri === typeof Array
+	)
+		throw new createHttpError[400]("Invalid URI");
+
+	try {
+		const inviteeRes = await getEventInvitee(uri);
+		res.status(200).json(inviteeRes);
+	} catch (err) {
+		console.error(err);
+		throw new createHttpError[500](
+			JSON.stringify({
+				message: "Error getting invitee",
+				error: err,
+			})
+		);
+	}
+};
+
+export default apiHandler({
+	GET: eventInviteeHandler,
+});

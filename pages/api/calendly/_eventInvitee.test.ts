2d0c52f53941e90f8e8c19696aecff58368970df
Kenton Duprey <kentonduprey@icloud.com>
2023-01-18 21:51:58 -0500

    Adding tests for eventInvitee

---
+++ pages/api/calendly/_eventInvitee.test.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { describe, expect, it, Mocked, vitest as vi } from "vitest";
+import { getInviteeResponse as data } from "../../../data/calendlyResponses/getInviteeResponse";
+import eventInvitee, { getEventInvitee } from "./eventInvitee";
+
+vi.mock("axios");
+const mockedAxios = axios as Mocked<typeof axios>;
+
+describe("eventInvitee should", () => {
+	it("Should handle a correct data submission", async () => {
+		mockedAxios.get.mockResolvedValueOnce({ data });
+
+		const response = await getEventInvitee("test");
+
+		expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+		expect(response).toEqual(data);
+	});
+
+	it("Should handle an incorrect data submission", async () => {
+		mockedAxios.get.mockRejectedValueOnce(
+			new Error("Error getting invitee")
+		);
+
+		try {
+			const response = await getEventInvitee("");
+			expect(response).toBeUndefined();
+		} catch (error) {
+			expect(error).toBeInstanceOf(Error);
+			expect(error).toEqual(Error("Invalid URI"));
+		}
+	});
+});

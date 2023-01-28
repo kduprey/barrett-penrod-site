b986d229a9ae6711a8673590987c0885c741ebdb
Kenton Duprey <kentonduprey@icloud.com>
2023-01-27 21:13:55 -0500

    Updating error handling in getZoomLink

--- utils/getZoomLink.ts
+++ utils/getZoomLink.ts
@@ -2,27 +2,25 @@ import { getEventInfo } from "../pages/api/calendly/eventInfo";
 import { InPersonLocation, ZoomLocation } from "../types/types";
 import { instanceOfZoomLocation } from "./isZoomLocation";
 
-const getZoomLink = async (eventURI: string): Promise<string | null> => {
+const getZoomLink = async (eventURI: string): Promise<string> => {
 	let location: ZoomLocation | InPersonLocation | undefined;
 	try {
 		const eventResponse = await getEventInfo(eventURI);
-		location = eventResponse.data.resource.location;
+		location = eventResponse.resource.location;
 	} catch (error) {
 		console.error(error);
-		return null;
+		throw new Error("Error getting event info");
 	}
 
 	// Make sure Zoom URL is available, if Zoom location
 	if (instanceOfZoomLocation(location) && location !== undefined) {
 		while ((location as ZoomLocation).status === "initiated") {
 			console.log("Zoom URL not available, retrying...");
-			location = await (
-				await getEventInfo(eventURI)
-			).data.resource.location;
+			location = (await getEventInfo(eventURI)).resource.location;
 		}
 		return (location as ZoomLocation).join_url;
 	}
-	return null;
+	throw new Error("No Zoom link available");
 };
 
 export default getZoomLink;

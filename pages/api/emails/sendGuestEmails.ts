0ac4f00c3bc05adf98f901bb66f2e46f8bca6a79
Kenton Duprey <kentonduprey@icloud.com>
2023-01-27 21:12:32 -0500

    Updating sendGuestEmails with new email params and API pattern

--- pages/api/emails/sendGuestEmails.ts
+++ pages/api/emails/sendGuestEmails.ts
@@ -1,23 +1,31 @@
 import { ClientResponse, MailDataRequired } from "@sendgrid/mail";
+import createHttpError from "http-errors";
 import type { NextApiRequest, NextApiResponse } from "next";
+import {
+	SessionLocation,
+	SessionLocations,
+	SessionType,
+	SessionTypes,
+} from "types/types";
+import apiHandler from "utils/api";
+import { validateRequest } from "utils/yup";
+import * as yup from "yup";
 import { sendgrid } from "../../../config";
-import { Contact, EmailTemplateData } from "../../../types/emailTypes";
-import { invalidMethod } from "../../../utils/responseDefaults";
+import {
+	clientSchema,
+	Contact,
+	EmailTemplateData,
+	GuestEmails,
+} from "../../../types/emailTypes";
 
 // Template Data
 // {
-//     "bookingDate": "2:00 pm",
+//     "bookingTime": "2:00 pm",
 //     "bookingDate": "June 26, 2022",
-//     "bookingName": "Voice Lesson - Virtual",
+//     "sessionType": "Voice Lesson",
+//     "bookingLocation": "Virtual",
 //     "zoomLink": "https://zoom.us/testlink"
 // }
-
-type GuestEmailsParams = {
-	guests: Contact[];
-	bookingDate: string;
-	bookingName: string;
-	zoomLink?: string;
-};
 
 /**
  * This endpoint is used to send a guest email to a list of guests whom are attending a session.
@@ -28,12 +36,49 @@ type GuestEmailsParams = {
  * @returns The response from SendGrid
  */
 
+const schema: yup.SchemaOf<GuestEmails> = yup.object({
+	guests: yup
+		.array(
+			yup
+				.object()
+				.shape({
+					email: yup
+						.string()
+						.email("Client email must be a valid email address")
+						.defined()
+						.required("Client email is required"),
+					name: yup.string(),
+				})
+				.defined()
+				.required()
+		)
+		.defined()
+		.required(),
+	bookingDate: yup.date().required(),
+	sessionType: yup
+		.mixed<SessionType>()
+		.oneOf([...SessionTypes])
+		.defined()
+		.required(),
+	bookingLocation: yup
+		.mixed<SessionLocation>()
+		.oneOf([...SessionLocations])
+		.defined()
+		.required(),
+	zoomLink: yup
+		.string()
+		.url(
+			"Please enter a valid Zoom link (e.g. https://us06web.zoom.us/j/xxxxxx)"
+		),
+});
+
 const sendGuestEmails = async ({
 	guests,
+	sessionType,
+	bookingLocation,
 	bookingDate,
-	bookingName,
 	zoomLink,
-}: GuestEmailsParams): Promise<[ClientResponse, {}]> => {
+}: GuestEmails): Promise<[ClientResponse, {}]> => {
 	const templateId: string = "d-b628680e34354157b553625c036e2836";
 
 	const message: MailDataRequired = {
@@ -59,9 +104,10 @@ const sendGuestEmails = async ({
 						day: "numeric",
 						year: "numeric",
 					}),
-					bookingName,
+					sessionType,
+					bookingLocation,
 					zoomLink,
-				} as EmailTemplateData,
+				},
 			},
 		],
 		templateId,
@@ -73,36 +119,30 @@ const sendGuestEmails = async ({
 		return response;
 	} catch (error: any) {
 		console.error(error);
-		return error;
+		throw new Error(error);
 	}
 };
 
 export { sendGuestEmails };
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-	invalidMethod("POST", req, res);
-
-	const { guests, bookingDate, bookingName, zoomLink } = req.body;
-
-	if (!guests || !bookingDate || !bookingName) {
-		res.status(400).json({
-			error: "Missing required fields",
-		});
-	}
+	const data = validateRequest(req.body, schema);
 
 	try {
-		const response = await sendGuestEmails({
-			guests,
-			bookingDate,
-			bookingName,
-			zoomLink,
-		});
+		const response = await sendGuestEmails(data as GuestEmails);
 		console.log(response);
-		res.status(200).json({ message: response });
+		res.status(200).json(response);
 	} catch (error: any) {
-		console.error(error.body);
-		res.status(500).json({ message: error });
+		console.error(error);
+		throw new createHttpError.InternalServerError(
+			JSON.stringify({
+				message: "There was an error sending the email.",
+				error,
+			})
+		);
 	}
 };
 
-export default handler;
+export default apiHandler({
+	POST: handler,
+});

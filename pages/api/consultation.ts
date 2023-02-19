969e2991ad2eb95c994d72650e31ccce203cf6b1
Kenton Duprey <kentonduprey@icloud.com>
2023-02-19 00:25:35 -0500

    Abstracting and restructuring logic for consultation bookings

--- pages/api/consultation.ts
+++ pages/api/consultation.ts
@@ -1,39 +1,62 @@
-import { clients } from "@prisma/client";
 import createHttpError from "http-errors";
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
-import Stripe from "stripe";
-import apiHandler from "utils/api";
-import getBookingLocation from "utils/getBookingLocation";
-import getZoomLink from "utils/getZoomLink";
-import { validateRequest } from "utils/yup";
+import { CalendlyEvent } from "types/calendlyTypes";
+import { CalendlyInvitee } from "types/types";
+import {
+	checkForClient,
+	createClient,
+	createStripeCustomer,
+	updateClient,
+} from "utils/consultationHelpers";
 import * as Yup from "yup";
-import { stripe } from "../../config";
-import { CalendlyEvent, Invitee, ZoomLocation } from "../../types/types";
-import { instanceOfZoomLocation } from "../../utils/isZoomLocation";
+import apiHandler from "../../utils/api";
+import getZoomLink from "../../utils/getZoomLink";
+import { validateRequest } from "../../utils/yup";
 import { getEventInfo } from "./calendly/eventInfo";
 import { getEventInvitee } from "./calendly/eventInvitee";
 import { sendConsultationEmail } from "./emails/sendConsultation";
-import { sendGuestEmails } from "./emails/sendGuestEmails";
 
 const consultationParams = Yup.object().shape({
 	eventURI: Yup.string().required("Event URI is required"),
 	inviteeURI: Yup.string().required("Invitee URI is required"),
 });
 
-const consultationHandler = async (eventURI: string, inviteeURI: string) => {
-	let existingCustomer: clients | null,
-		zoomLink: string,
-		event: CalendlyEvent,
-		invitee: Invitee;
+const consultationHandler = async (
+	eventURI: string,
+	inviteeURI: string
+): Promise<string> => {
+	let zoomLink: string;
+	let event: CalendlyEvent;
+	let invitee: CalendlyInvitee;
+	let calendlyEventPayloadId: string;
 
 	try {
+		console.info("Getting Calendly event data");
 		// Get Calendly event data
-		event = (await getEventInfo(eventURI)).resource;
+		event = await getEventInfo(eventURI);
 		// Get invitee data
-		invitee = (await getEventInvitee(inviteeURI)).resource;
-	} catch (err: any) {
+		invitee = await getEventInvitee(inviteeURI);
+	} catch (err: unknown) {
 		console.error(err);
-		throw new Error("Error getting consultation data", err);
+		if (err instanceof Error)
+			throw new Error("Error getting consultation data", err);
+		else throw new Error("Error getting consultation data");
+	}
+
+	try {
+		// Get Calendly event payload ID
+		const dbItem = await prisma.calendlyInviteePayloads.findUnique({
+			where: {
+				uri: event.resource.uri,
+			},
+		});
+		if (!dbItem) throw new Error("Calendly event payload ID not found");
+		calendlyEventPayloadId = dbItem?.id;
+	} catch (err: unknown) {
+		console.error(err);
+		if (err instanceof Error)
+			throw new Error("Error getting Calendly event payload ID", err);
+		else throw new Error("Error getting Calendly event payload ID");
 	}
 
 	// Get Zoom link
@@ -45,89 +68,37 @@ const consultationHandler = async (eventURI: string, inviteeURI: string) => {
 	}
 
 	// Check if user is client in database
-	try {
-		// Search by name
-		existingCustomer = await prisma.clients.findUnique({
-			where: {
-				name: event.name,
-			},
-		});
-		// If no results, seach by email
-		if (!existingCustomer) {
-			existingCustomer = await prisma.clients.findUnique({
-				where: {
-					email: invitee.email,
-				},
-			});
-		}
-	} catch (err) {
-		console.warn(err, "DB error");
-		throw new Error("Error searching DB for existing customer");
-	}
+	const existingCustomer = await checkForClient(
+		invitee.resource.name,
+		invitee.resource.email
+	);
 
+	// If user is already a client, update their data
 	if (existingCustomer)
-		try {
-			await prisma.clients.update({
-				where: {
-					id: existingCustomer.id,
-				},
-				data: {
-					nextLesson: event.start_time,
-					activeMember: true,
-					archived: false,
-					lessonsRemaining: existingCustomer.lessonsRemaining + 1,
-				},
-			});
-		} catch (err) {
-			console.error(err);
-			throw new Error("Error updating existing customer");
-		}
+		await updateClient(existingCustomer, event, calendlyEventPayloadId);
+	else {
+		// If not, create user in database and Stripe
 
-	// If not, create user in database and Stripe
-	if (!existingCustomer) {
-		let newCustomer: Stripe.Customer;
-		// Create new Stripe customer
-		try {
-			newCustomer = await stripe.customers.create({
-				name: invitee.name,
-				email: invitee.email,
-			});
-		} catch (error: any) {
-			console.error(error, { invitee, event });
-			throw new Error("Error creating new Stripe customer", error);
-		}
-		// Create new client in database
-		try {
-			await prisma.clients.create({
-				data: {
-					activeMember: true,
-					archived: false,
-					email: invitee.email,
-					dateJoined: event.start_time,
-					firstLesson: event.start_time,
-					nextLesson: event.start_time,
-					lessonsRemaining: 1,
-					name: event.name,
-					stripe_customer_id: newCustomer.id,
-					totalSpend: 0,
-				},
-			});
-		} catch (err: any) {
-			console.error(err, { invitee, event });
-			throw new Error("Error creating new client in DB", err);
-		}
+		const stripeCustomer = await createStripeCustomer(invitee);
+		await createClient(
+			event,
+			invitee,
+			stripeCustomer,
+			calendlyEventPayloadId
+		);
 	}
 
 	// Send consultation email
 	try {
 		await sendConsultationEmail({
 			client: {
-				name: invitee.name,
-				email: invitee.email,
+				name: invitee.resource.name,
+				email: invitee.resource.email,
 			},
-			bookingDate: new Date(event.start_time),
+			bookingDate: new Date(event.resource.start_time),
 			zoomLink,
 		});
+		return "Consultation email sent and client created";
 	} catch (err) {
 		console.error(err);
 		throw new Error("Error sending consultation email");
@@ -140,13 +111,12 @@ const handler: NextApiHandler = async (
 	req: NextApiRequest,
 	res: NextApiResponse
 ) => {
-	const data = validateRequest(req.body, consultationParams);
-
+	const { eventURI, inviteeURI } = validateRequest(
+		req.body,
+		consultationParams
+	);
 	try {
-		const response = await consultationHandler(
-			data.eventURI,
-			data.inviteeURI
-		);
+		const response = await consultationHandler(eventURI, inviteeURI);
 		return res.status(200).json(response);
 	} catch (error) {
 		console.error(error);

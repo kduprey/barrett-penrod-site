60964acf6c5441bbe83772d673009fce0d87ce11
Kenton Duprey <kentonduprey@icloud.com>
2023-02-20 20:04:10 -0500

    update server

--- config/server.ts
+++ config/server.ts
@@ -1,5 +1,5 @@
 import { dev } from "./dev";
 
 export const server = dev
-	? "https://f664-2600-4040-9a9b-9200-8052-8ae7-ccd5-5e42.ngrok.io/"
+	? "https://d6ef-2600-4040-9a9b-9200-8052-8ae7-ccd5-5e42.ngrok.io"
 	: "https://barrettpenrod.com";

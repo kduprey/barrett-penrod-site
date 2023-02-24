69aa262cd344f66817be49495e9390fd61d28808
Kenton Duprey <kentonduprey@icloud.com>
2023-02-24 17:56:48 -0500

    Updating regrex on isPackageCheckout

--- utils/isPackageCheckout.ts
+++ utils/isPackageCheckout.ts
@@ -5,7 +5,7 @@ const isPackageCheckout = async (
 	session: Stripe.Checkout.Session
 ): Promise<boolean> => {
 	const lineItems = await stripe.checkout.sessions.listLineItems(session.id);
-	const packageRegex = new RegExp(/Package$/i);
+	const packageRegex = new RegExp(/Package$/, "i");
 
 	return lineItems.data
 		.map((item) => item.description)

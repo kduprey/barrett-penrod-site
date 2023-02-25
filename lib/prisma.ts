cc3172ce0074126a6c111387f3c823979a3226f0
Kenton Duprey <kentonduprey@icloud.com>
2023-02-24 19:28:21 -0500

    Updating global configuration for prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import { dev } from "config/dev";
 import { prismaConfig } from "../config";
 
-declare global {
-	let prisma: PrismaClient;
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 // Prevent multiple instances of Prisma Client in development
 // eslint-disable-next-line
 // @ts-ignore
-const prisma = global.prisma || new PrismaClient({ ...prismaConfig });
+const prisma = globalForPrisma.prisma || new PrismaClient({ ...prismaConfig });
+if (dev) globalForPrisma.prisma = prisma;
 
 export default prisma;

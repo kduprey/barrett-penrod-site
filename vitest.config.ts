1f168b047c19afb1ceab614041056d40d7d00d5b
Kenton Duprey <kentonduprey@icloud.com>
2023-03-05 18:03:43 -0500

    Updating vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	resolve: {
+		alias: {
+			"~/": `${path.resolve(__dirname, "src")}/`,
+		},
+	},
 	plugins: [react()],
 	test: {
 		coverage: {

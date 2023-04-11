3fd5982e7c2a23fdc758be78a18fd26d859a4eda
Kenton Duprey <kentonduprey@icloud.com>
2023-04-10 23:42:30 -0400

    Trying env name in lowercase for actions

--- config/hygraphCMS.ts
+++ config/hygraphCMS.ts
@@ -4,7 +4,7 @@ const hygraphcms: GraphQLClient = new GraphQLClient(
 	"https://api-us-east-1.hygraph.com/v2/cl7ep7a6t60k301ul5b4o8j9h/master",
 	{
 		headers: {
-			Authorization: `Bearer ${process.env["HYGRAPH_API_KEY"]}`,
+			Authorization: `Bearer ${process.env.HYGRAPH_API_KEY}`,
 		},
 	}
 );

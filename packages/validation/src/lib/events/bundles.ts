da6cad0c51ad8b689ca2ba6005165af13a251d13
Kenton Duprey <kentonduprey@icloud.com>
2023-04-24 02:04:59 -0400

    Fixed import of package types in validation

--- packages/validation/src/lib/events/bundles.ts
+++ packages/validation/src/lib/events/bundles.ts
@@ -1,4 +1,4 @@
-import { PACKAGE_TYPES } from "@bpvs/packages/types/src";
+import { PACKAGE_TYPES } from "@bpvs/types";
 import { z } from "zod";
 
 export const PackageTypeEnum = z.enum(PACKAGE_TYPES);

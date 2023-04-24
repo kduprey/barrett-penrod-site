f1c86c90b20308ebb393a892ac8879d4dc513c2f
Kenton Duprey <kentonduprey@icloud.com>
2023-04-24 02:04:20 -0400

    Updating @bpvs/libs

--- packages/libs/src/lib/libs.ts
+++ packages/libs/src/lib/libs.ts
@@ -1,4 +1,6 @@
 import { dev } from "@bpvs/types";
+import sendgridClient from "@sendgrid/mail";
+import { GraphQLClient } from "graphql-request";
 import Stripe from "stripe";
 
 export const stripe = new Stripe(
@@ -11,6 +13,30 @@ export const stripe = new Stripe(
 		appInfo: {
 			name: "Booking Site",
 			url: "https://barrettpendrod.com/bookings",
+		},
+	}
+);
+
+export const server = dev
+	? "https://ad14-2600-4040-9a9b-9200-2582-1bd4-300-a0fb.ngrok.io"
+	: "https://barrettpenrod.com";
+
+export const stripeMode = dev ? "test" : "live";
+
+export const db = dev ? "test" : "production";
+
+if (dev) {
+	sendgridClient.setApiKey(process.env.SENDGRID_DEV_API_KEY as string);
+} else {
+	sendgridClient.setApiKey(process.env.SENDGRID_API_KEY as string);
+}
+export const sendgrid = sendgridClient;
+
+export const hygraphcms: GraphQLClient = new GraphQLClient(
+	"https://api-us-east-1.hygraph.com/v2/cl7ep7a6t60k301ul5b4o8j9h/master",
+	{
+		headers: {
+			Authorization: `Bearer ${process.env.HYGRAPH_API_KEY}`,
 		},
 	}
 );

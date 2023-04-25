b64b5b5f2ac67e2eee8a68a3ff7ccd3484d06cc9
Kenton Duprey <kentonduprey@icloud.com>
2023-04-24 22:39:53 -0400

    Updated db logic for prisma instance

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -3,7 +3,11 @@ import { dev } from "@bpvs/types";
 import { PrismaClient } from "@prisma/client";
 import { PrismaClientOptions } from "@prisma/client/runtime/library";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+declare global {
+	// allow global `var` declarations
+	// eslint-disable-next-line no-var
+	var prisma: PrismaClient | undefined;
+}
 
 export const prismaConfig =
 	dev && !process.env["TEST_ENV"]
@@ -16,6 +20,16 @@ export const prismaConfig =
 				errorFormat: "minimal" as PrismaClientOptions["errorFormat"],
 		  };
 
-export const prisma =
-	globalForPrisma.prisma || new PrismaClient({ ...prismaConfig });
-if (dev) globalForPrisma.prisma = prisma;
+let prisma: PrismaClient;
+
+if (typeof window === "undefined") {
+	if (process.env.NODE_ENV === "production") {
+		prisma = new PrismaClient(dev ? { ...prismaConfig } : undefined);
+	} else {
+		if (!global.prisma) {
+			global.prisma = new PrismaClient(dev ? { ...prismaConfig } : undefined);
+		}
+
+		prisma = global.prisma;
+	}
+}

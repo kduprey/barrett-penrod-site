5e8209565735f57f4319501f3da47826a23d0b85
Kenton Duprey <kentonduprey@icloud.com>
2023-04-30 15:34:44 -0400

    Updating statc assets imports

--- apps/site/src/components/Bookings/Bundles.tsx
+++ apps/site/src/components/Bookings/Bundles.tsx
@@ -4,7 +4,7 @@ import BundleModal from "./BundleModal";
 
 const Bundles = () => {
 	const [isBundleModalOpen, setIsBundleModalOpen] = useState(false);
-	const [selectedBundle, setselectedBundle] = useState<number>();
+	const [selectedBundle, setselectedBundle] = useState<number>(0);
 
 	return (
 		<section>
@@ -18,34 +18,30 @@ const Bundles = () => {
 			</h2>
 			<div className="grid gap-6 sm:grid-cols-2">
 				{bundles.map((bundle, index) => (
-						<div
-							key={bundle.title}
-							className="withTransition bg-secondary flex max-w-sm flex-col items-center justify-center space-y-3 rounded-lg p-6 shadow-md hover:scale-105 hover:opacity-90"
+					<div
+						key={bundle.title}
+						className="withTransition bg-secondary flex max-w-sm flex-col items-center justify-center space-y-3 rounded-lg p-6 shadow-md hover:scale-105 hover:opacity-90"
+						onClick={() => {
+							setIsBundleModalOpen(true);
+							setselectedBundle(index);
+						}}
+					>
+						<h4 className="text-primary text-center">{bundle.title}</h4>
+						<p className="text-primary text-center">{bundle.discount}</p>
+						<p className="text-primary text-center text-3xl font-bold">
+							${bundle.price}
+						</p>
+						<button
+							className="bg-primary text-secondary cursor-pointer"
 							onClick={() => {
 								setIsBundleModalOpen(true);
 								setselectedBundle(index);
 							}}
 						>
-							<h4 className="text-primary text-center">
-								{bundle.title}
-							</h4>
-							<p className="text-primary text-center">
-								{bundle.discount}
-							</p>
-							<p className="text-primary text-center text-3xl font-bold">
-								${bundle.price}
-							</p>
-							<button
-								className="bg-primary text-secondary cursor-pointer"
-								onClick={() => {
-									setIsBundleModalOpen(true);
-									setselectedBundle(index);
-								}}
-							>
-								Reserve Now!
-							</button>
-						</div>
-					))}
+							Reserve Now!
+						</button>
+					</div>
+				))}
 			</div>
 		</section>
 	);
--- apps/site/src/pages/bookings/bundles.tsx
+++ apps/site/src/pages/bookings/bundles.tsx
@@ -6,7 +6,7 @@ import Logo from "../../components/Logo";
 
 const Bundles: NextPageWithLayout = () => {
 	const [isOpen, setIsOpen] = useState(false);
-	const [selectedBundle, setselectedBundle] = useState<number>();
+	const [selectedBundle, setselectedBundle] = useState<number>(0);
 
 	return (
 		<section className="flex flex-col items-center p-4">
@@ -19,35 +19,29 @@ const Bundles: NextPageWithLayout = () => {
 			<div className="mx-auto max-w-[18rem]">
 				<Logo />
 			</div>
-			<h2 className="text-secondary py-6 text-center">
-				Discount Packages
-			</h2>
+			<h2 className="text-secondary py-6 text-center">Discount Packages</h2>
 			<div className="flex w-full max-w-screen-lg  flex-row flex-wrap items-center justify-evenly">
 				{bundles.map((bundle, index) => (
-						<div
-							key={bundle.title}
-							className="withTransition bg-secondary m-2 flex flex-col items-center justify-center space-y-3 rounded-lg p-6 shadow-md hover:scale-105 hover:opacity-90 md:w-[18rem] lg:w-[22rem]"
+					<div
+						key={bundle.title}
+						className="withTransition bg-secondary m-2 flex flex-col items-center justify-center space-y-3 rounded-lg p-6 shadow-md hover:scale-105 hover:opacity-90 md:w-[18rem] lg:w-[22rem]"
+					>
+						<h4 className="text-primary text-center">{bundle.title}</h4>
+						<p className="text-primary text-center">{bundle.discount}</p>
+						<p className="text-primary text-center text-3xl font-bold">
+							${bundle.price}
+						</p>
+						<button
+							className="bg-primary text-secondary cursor-pointer"
+							onClick={() => {
+								setIsOpen(true);
+								setselectedBundle(index);
+							}}
 						>
-							<h4 className="text-primary text-center">
-								{bundle.title}
-							</h4>
-							<p className="text-primary text-center">
-								{bundle.discount}
-							</p>
-							<p className="text-primary text-center text-3xl font-bold">
-								${bundle.price}
-							</p>
-							<button
-								className="bg-primary text-secondary cursor-pointer"
-								onClick={() => {
-									setIsOpen(true);
-									setselectedBundle(index);
-								}}
-							>
-								Reserve Now!
-							</button>
-						</div>
-					))}
+							Reserve Now!
+						</button>
+					</div>
+				))}
 			</div>
 		</section>
 	);

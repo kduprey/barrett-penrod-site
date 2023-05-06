c06f573a6c3b95322936d910a34f4d0fa8ee076d
Kenton Duprey <kentonduprey@icloud.com>
2023-05-06 07:55:15 -0400

    Removing root site

--- __mocks__/axios.ts
+++
@@ -1,9 +0,0 @@
-import { vi } from "vitest";
-
-const mockAxios = {
-	get: vi.fn(),
-	post: vi.fn(),
-	delete: vi.fn(),
-};
-
-export default mockAxios;
--- src/data/calendlyResponses/cancelEventResponse.ts
+++
@@ -1,9 +0,0 @@
-import { CalendlyCancel } from "types/types";
-
-export const cancelEventResponse: CalendlyCancel = {
-	resource: {
-		canceled_by: "string",
-		reason: "string",
-		canceler_type: "host",
-	},
-};

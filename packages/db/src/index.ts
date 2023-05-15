256b612d29e07faf5df96484a0b38bc7a5721308
Kenton Duprey <kentonduprey@icloud.com>
2023-05-14 23:33:53 -0400

    Updating db to having current mongodb schema along with new Postgres schema

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -8,32 +8,32 @@ export * from "./client";
 export { prisma };
 
 declare global {
-	// allow global `var` declarations
-	// eslint-disable-next-line no-var
-	var prisma: PrismaClient | undefined;
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
 }
 
 export const prismaConfig =
-	dev && !process.env["TEST_ENV"]
-		? {
-				log: ["query", "info", "warn", "error"] as PrismaClientOptions["log"],
-				errorFormat: "pretty" as PrismaClientOptions["errorFormat"],
-		  }
-		: {
-				log: ["query", "info", "warn", "error"] as PrismaClientOptions["log"],
-				errorFormat: "minimal" as PrismaClientOptions["errorFormat"],
-		  };
+  dev && !process.env["TEST_ENV"]
+    ? {
+        log: ["query", "info", "warn", "error"] as PrismaClientOptions["log"],
+        errorFormat: "pretty" as PrismaClientOptions["errorFormat"],
+      }
+    : {
+        log: ["query", "info", "warn", "error"] as PrismaClientOptions["log"],
+        errorFormat: "minimal" as PrismaClientOptions["errorFormat"],
+      };
 
 let prisma: PrismaClient;
 
 if (typeof window === "undefined") {
-	if (process.env.NODE_ENV === "production") {
-		prisma = new PrismaClient(dev ? { ...prismaConfig } : undefined);
-	} else {
-		if (!global.prisma) {
-			global.prisma = new PrismaClient(dev ? { ...prismaConfig } : undefined);
-		}
+  if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient(dev ? { ...prismaConfig } : undefined);
+  } else {
+    if (!global.prisma) {
+      global.prisma = new PrismaClient(dev ? { ...prismaConfig } : undefined);
+    }
 
-		prisma = global.prisma;
-	}
+    prisma = global.prisma;
+  }
 }

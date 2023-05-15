16b27e221722cb0272524b1504ec47e65e17d929
Kenton Duprey <kentonduprey@icloud.com>
2023-05-14 23:15:31 -0400

    Updating eslint-config-bpvs with respective plugins

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,31 +1,31 @@
 module.exports = {
-	extends: ["next", "turbo", "prettier"],
-	rules: {
-		"@next/next/no-html-link-for-pages": "off",
-		quotes: ["error", "double"],
-		"arrow-body-style": ["error", "as-needed"],
-		"react/self-closing-comp": [
-			"error",
-			{
-				component: true,
-				html: true,
-			},
-		],
-	},
-	plugins: ["@typescript-eslint", "eslint-plugin-tsdoc", "vitest"],
-	overrides: [
-		{
-			files: ["__tests__/**/*.test.{js,ts,jsx,tsx}"],
-			rules: {
-				"@typescript-eslint/unbound-method": "off",
-			},
-			extends: ["plugin:vitest/recommended"],
-		},
-	],
-	parserOptions: {
-		babelOptions: {
-			presets: [require.resolve("next/babel")],
-		},
-	},
-	ignorePatterns: [".eslintrc.js"],
+  extends: ["next", "turbo", "prettier"],
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+    quotes: ["error", "double"],
+    "arrow-body-style": ["error", "as-needed"],
+    "react/self-closing-comp": [
+      "error",
+      {
+        component: true,
+        html: true,
+      },
+    ],
+  },
+  plugins: ["@typescript-eslint", "eslint-plugin-tsdoc", "vitest"],
+  overrides: [
+    {
+      files: ["__tests__/**/*.test.{js,ts,jsx,tsx}"],
+      rules: {
+        "@typescript-eslint/unbound-method": "off",
+      },
+      extends: ["plugin:vitest/recommended"],
+    },
+  ],
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve("next/babel")],
+    },
+  },
+  ignorePatterns: [".eslintrc.js"],
 };

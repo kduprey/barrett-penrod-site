31fd42659db4411ab1c7d2a857f8da4e897c09aa
Kenton Duprey <kentonduprey@icloud.com>
2023-05-16 14:38:40 -0400

    Updating vite path aliases

--- apps/site/vitest.config.ts
+++ apps/site/vitest.config.ts
@@ -4,42 +4,45 @@ import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	test: {
-		coverage: {
-			reporter: ["text", "json", "html", "lcov"],
-			provider: "c8",
-			exclude: [
-				"coverage/**",
-				"dist/**",
-				"packages/*/test{,s}/**",
-				"**/*.d.ts",
-				"cypress/**",
-				"test{,s}/**",
-				"test{,-*}.{js,cjs,mjs,ts,tsx,jsx}",
-				"**/*{.,-}test.{js,cjs,mjs,ts,tsx,jsx}",
-				"**/*{.,-}spec.{js,cjs,mjs,ts,tsx,jsx}",
-				"**/__tests__/**",
-				"**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress}.config.*",
-				"**/.{eslint,mocha,prettier}rc.{js,cjs,yml}",
-			],
-			include: [
-				"pages/api/*.ts",
-				"pages/api/**/*.ts",
-				"utils/**",
-				"utils/**/*.ts",
-			],
-		},
-		environment: "jsdom",
-		globals: true,
-		isolate: true,
-		threads: false,
-	},
-	resolve: {
-		alias: {
-			pages: path.resolve(__dirname, "src/pages"),
-			data: path.resolve(__dirname, "src/data"),
-			utils: path.resolve(__dirname, "src/utils"),
-		},
-	},
+  plugins: [react()],
+  test: {
+    coverage: {
+      reporter: ["text", "json", "html", "lcov"],
+      provider: "c8",
+      exclude: [
+        "coverage/**",
+        "dist/**",
+        "packages/*/test{,s}/**",
+        "**/*.d.ts",
+        "cypress/**",
+        "test{,s}/**",
+        "test{,-*}.{js,cjs,mjs,ts,tsx,jsx}",
+        "**/*{.,-}test.{js,cjs,mjs,ts,tsx,jsx}",
+        "**/*{.,-}spec.{js,cjs,mjs,ts,tsx,jsx}",
+        "**/__tests__/**",
+        "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress}.config.*",
+        "**/.{eslint,mocha,prettier}rc.{js,cjs,yml}",
+      ],
+      include: [
+        "pages/api/*.ts",
+        "pages/api/**/*.ts",
+        "utils/**",
+        "utils/**/*.ts",
+      ],
+    },
+    environment: "jsdom",
+    globals: true,
+    isolate: true,
+    threads: false,
+  },
+  resolve: {
+    alias: {
+      "@bpvs/config": path.resolve(__dirname, "packages/config"),
+      "@bpvs/site": path.resolve(__dirname, "apps/site"),
+      "@bpvs/utils": path.resolve(__dirname, "packages/utils"),
+      "@bpvs/db": path.resolve(__dirname, "packages/db"),
+      "@bpvs/types": path.resolve(__dirname, "packages/types"),
+      "@bpvs/validation": path.resolve(__dirname, "packages/validation"),
+    },
+  },
 });

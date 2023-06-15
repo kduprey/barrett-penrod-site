65d19e0a60e52fd6b9388dcb369ea57ce2bf739d
Kenton Duprey <kentonduprey@icloud.com>
2023-06-14 23:10:07 -0400

    Removing header from PageWrapper.tsx

--- apps/admin/src/components/PageWrapper/PageWrapper.tsx
+++ apps/admin/src/components/PageWrapper/PageWrapper.tsx
@@ -27,25 +27,6 @@ export const PageWrapper = ({ children }: PropsWithChildren) => {
       navbarOffsetBreakpoint="sm"
       asideOffsetBreakpoint="sm"
       navbar={<NavbarNested />}
-      header={
-        <Header height={{ base: 50, md: 70 }} p="md">
-          <div
-            style={{ display: "flex", alignItems: "center", height: "100%" }}
-          >
-            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
-              <Burger
-                opened={opened}
-                onClick={() => setOpened((o) => !o)}
-                size="sm"
-                color={theme.colors.gray[6]}
-                mr="xl"
-              />
-            </MediaQuery>
-
-            <Text>Application header</Text>
-          </div>
-        </Header>
-      }
     >
       {children}
     </AppShell>

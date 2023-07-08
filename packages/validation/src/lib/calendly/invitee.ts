92f18ddbd37100042168de8f684cbec0884e3d2d
Kenton Duprey <kentonduprey@icloud.com>
2023-07-08 14:15:14 -0400

    Refactored validation to match updated types

--- packages/validation/src/lib/calendly/invitee.ts
+++ packages/validation/src/lib/calendly/invitee.ts
@@ -2,89 +2,96 @@ import { z } from "zod";
 import { cancellationSchema } from "./event";
 
 export const reconfirmationSchema = z.object({
-	created_at: z.string(),
-	confirmed_at: z.coerce.date(),
+  created_at: z.string(),
+  confirmed_at: z.coerce.date(),
 });
 
 export const noShowSchema = z.object({
-	uri: z.string(),
-	created_at: z.coerce.date(),
+  uri: z.string(),
+  created_at: z.coerce.date(),
 });
 
 export const paymentSchema = z.object({
-	external_id: z.string(),
-	provider: z.enum(["stripe", "paypal"]),
-	amount: z.number(),
-	currency: z.union([
-		z.literal("AUD"),
-		z.literal("CAD"),
-		z.literal("EUR"),
-		z.literal("GBP"),
-		z.literal("USD"),
-	]),
-	terms: z.string(),
-	successful: z.boolean(),
+  external_id: z.string(),
+  provider: z.enum(["stripe", "paypal"]),
+  amount: z.number(),
+  currency: z.union([
+    z.literal("AUD"),
+    z.literal("CAD"),
+    z.literal("EUR"),
+    z.literal("GBP"),
+    z.literal("USD"),
+  ]),
+  terms: z.string(),
+  successful: z.boolean(),
+});
+
+export const questionSchema = z.object({
+  question: z.string(),
+  answer: z.string().optional(),
+  position: z.number(),
 });
 
 export const firstLessonQuestionSchema = z.object({
-	question: z.literal("Is this your first lesson with Barrett?"),
-	answer: z.union([z.literal("Yes"), z.literal("No")]),
-	position: z.number(),
+  question: z.literal("Is this your first lesson with Barrett?"),
+  // This will be a string, but it will be either "Yes" or "No"
+  answer: z.string().optional(),
+  position: z.number(),
 });
 
 export const additionalInfoQuestionSchema = z.object({
-	question: z.literal(
-		"Please share anything that will help prepare for our meeting."
-	),
-	answer: z.string().optional(),
-	position: z.number(),
+  question: z.literal(
+    "Please share anything that will help prepare for our meeting."
+  ),
+  answer: z.string().optional(),
+  position: z.number(),
 });
 
+export const questionsAndAnswersSchema = z.array(questionSchema);
+
 export const trackingSchema = z.object({
-	utm_campaign: z.string().nullable(),
-	utm_source: z.string().nullable(),
-	utm_medium: z.string().nullable(),
-	utm_content: z.string().nullable(),
-	utm_term: z.string().nullable(),
-	salesforce_uuid: z.string().nullable(),
+  utm_campaign: z.string().nullable(),
+  utm_source: z.string().nullable(),
+  utm_medium: z.string().nullable(),
+  utm_content: z.string().nullable(),
+  utm_term: z.string().nullable(),
+  salesforce_uuid: z.string().nullable(),
 });
 
 export const calendlyCancelSchema = z.object({
-	resource: z.object({
-		canceled_by: z.string(),
-		reason: z.string(),
-		canceler_type: z.union([z.literal("host"), z.literal("invitee")]),
-	}),
+  resource: z.object({
+    canceled_by: z.string(),
+    reason: z.string(),
+    canceler_type: z.union([z.literal("host"), z.literal("invitee")]),
+  }),
 });
 
 export const calendlyInviteeResourceSchema = z.object({
-	cancel_url: z.string(),
-	created_at: z.string(),
-	email: z.string(),
-	event: z.string(),
-	first_name: z.string().nullable(),
-	last_name: z.string().nullable(),
-	name: z.string(),
-	new_invitee: z.string().nullable(),
-	no_show: noShowSchema.nullable(),
-	old_invitee: z.string().nullable(),
-	payment: paymentSchema.nullable(),
-	questions_and_answers: z.array(
-		z.union([firstLessonQuestionSchema, additionalInfoQuestionSchema])
-	),
-	reconfirmation: reconfirmationSchema.nullable(),
-	reschedule_url: z.string(),
-	rescheduled: z.boolean(),
-	routing_form_submission: z.string().nullable(),
-	status: z.union([z.literal("active"), z.literal("cancelled")]),
-	text_reminder_number: z.string().nullable(),
-	timezone: z.string(),
-	tracking: trackingSchema,
-	updated_at: z.string(),
-	uri: z.string(),
-	cancellation: cancellationSchema.optional(),
+  cancel_url: z.string(),
+  created_at: z.string(),
+  email: z.string(),
+  event: z.string(),
+  first_name: z.string().nullable(),
+  last_name: z.string().nullable(),
+  name: z.string(),
+  new_invitee: z.string().nullable(),
+  no_show: noShowSchema.nullable(),
+  old_invitee: z.string().nullable(),
+  payment: paymentSchema.nullable(),
+  questions_and_answers: questionsAndAnswersSchema,
+  reconfirmation: reconfirmationSchema.nullable(),
+  reschedule_url: z.string(),
+  rescheduled: z.boolean(),
+  routing_form_submission: z.string().nullable(),
+  status: z.union([z.literal("active"), z.literal("cancelled")]),
+  text_reminder_number: z.string().nullable(),
+  timezone: z.string(),
+  tracking: trackingSchema,
+  updated_at: z.string(),
+  uri: z.string(),
+  cancellation: cancellationSchema.optional(),
 });
 
 export const calendlyInviteeSchema = z.object({
-	resource: calendlyInviteeResourceSchema,
+  resource: calendlyInviteeResourceSchema,
 });
--- packages/validation/src/lib/calendly/webhooks.ts
+++ packages/validation/src/lib/calendly/webhooks.ts
@@ -1,38 +1,75 @@
 import { z } from "zod";
+import { calendlyEventResourceSchema, cancellationSchema } from "./event";
+import {
+  noShowSchema,
+  paymentSchema,
+  questionsAndAnswersSchema,
+  reconfirmationSchema,
+  trackingSchema,
+} from "./invitee";
 
 export const calendlyWebhookSchema = z.object({
-	uri: z.string(),
-	callback_url: z.string(),
-	created_at: z.string(),
-	updated_at: z.string(),
-	retry_started_at: z.string().nullable(),
-	state: z.union([z.literal("active"), z.literal("disabled")]),
-	events: z.array(
-		z.union([
-			z.literal("invitee.created"),
-			z.literal("invitee.canceled"),
-			z.literal("routing_form_submission.created"),
-		])
-	),
-	scope: z.union([z.literal("user"), z.literal("organization")]),
-	organization: z.string(),
-	user: z.string(),
-	creator: z.string(),
+  uri: z.string(),
+  callback_url: z.string(),
+  created_at: z.string(),
+  updated_at: z.string(),
+  retry_started_at: z.string().nullable(),
+  state: z.union([z.literal("active"), z.literal("disabled")]),
+  events: z.array(
+    z.union([
+      z.literal("invitee.created"),
+      z.literal("invitee.canceled"),
+      z.literal("routing_form_submission.created"),
+    ])
+  ),
+  scope: z.union([z.literal("user"), z.literal("organization")]),
+  organization: z.string(),
+  user: z.string(),
+  creator: z.string(),
 });
 
 export const calendlyPaginationSchema = z.object({
-	count: z.number(),
-	next_page: z.string().nullable(),
-	previous_page: z.string().nullable(),
-	next_page_token: z.string().nullable(),
-	previous_page_token: z.string().nullable(),
+  count: z.number(),
+  next_page: z.string().nullable(),
+  previous_page: z.string().nullable(),
+  next_page_token: z.string().nullable(),
+  previous_page_token: z.string().nullable(),
 });
 
 export const calendlyGetWebhookSchema = z.object({
-	collection: z.array(calendlyWebhookSchema),
-	pagination: calendlyPaginationSchema,
+  collection: z.array(calendlyWebhookSchema),
+  pagination: calendlyPaginationSchema,
 });
 
 export const calendlyPostWebhookSchema = z.object({
-	resource: calendlyWebhookSchema,
+  resource: calendlyWebhookSchema,
+});
+
+export const calendlyPayloadDataSchema = z.object({
+  cancel_url: z.string(),
+  // Date
+  created_at: z.union([z.date(), z.string()]),
+  email: z.string(),
+  event: z.string(),
+  name: z.string(),
+  first_name: z.string().nullable(),
+  last_name: z.string().nullable(),
+  new_invitee: z.string().nullable(),
+  old_invitee: z.string().nullable(),
+  questions_and_answers: questionsAndAnswersSchema,
+  reschedule_url: z.string(),
+  rescheduled: z.boolean(),
+  routing_form_submission: z.string().nullable(),
+  status: z.union([z.literal("active"), z.literal("canceled")]),
+  text_reminder_number: z.string().nullable(),
+  timezone: z.string().nullable(),
+  tracking: trackingSchema,
+  updated_at: z.union([z.date(), z.string()]),
+  uri: z.string(),
+  canceled: z.boolean(),
+  cancellation: cancellationSchema.optional(),
+  payment: paymentSchema.optional(),
+  no_show: noShowSchema,
+  reconfirmation: reconfirmationSchema.optional(),
+  scheduled_event: calendlyEventResourceSchema,
 });

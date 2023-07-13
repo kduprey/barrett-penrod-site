3c93fbb15f6573d07f0224e3c2f45029d6166e5b
Kenton Duprey <kentonduprey@icloud.com>
2023-07-12 20:57:41 -0400

    Fixing prisma with a monorepo on the admin

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -1,9 +1,19 @@
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ["@bpvs/config", "@bpvs/types", "@bpvs/db"],
   reactStrictMode: true,
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
   },
 };
 

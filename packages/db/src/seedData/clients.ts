e9661e202dd9c6373525d166cbc5875d3eac1614
Kenton Duprey <kentonduprey@icloud.com>
2023-07-12 21:12:32 -0400

    Adding seed data for clients and qr_code_logs

---
+++ packages/db/src/seedData/clients.ts
@@ -0,0 +1,22 @@
+import { Prisma } from "@bpvs/db";
+
+export const clients: Prisma.clientsCreateInput[] = [
+  {
+    activeMember: true,
+    archived: false,
+    email: "test@email.com",
+    dateJoined: "2001-01-01T05:00:00.000Z",
+    firstLesson: "2022-06-27T17:30:00.000Z",
+    lastLesson: "2022-07-11T17:30:00.000Z",
+    nextLesson: "2022-10-24T04:00:00.000Z",
+    lessonsRemaining: 1,
+    name: "Test User 1",
+    notes: "Update schedule policy ",
+    preferredLessonFormat: "Virtual",
+    pronouns: "She/They",
+    refundedVolume: 0,
+    totalLessons: 5,
+    totalSpend: 400,
+    stripe_customer_id: "cus_NNRmKpeWKc8i07",
+  },
+];
---
+++ packages/db/src/seedData/qr_code_logs.ts
@@ -0,0 +1,25 @@
+import { Prisma } from "../client";
+
+export const qr_code_logs: Prisma.qr_code_logsCreateManyInput[] = [
+  {
+    flyerSize: 3,
+    campaign: 1,
+    designId: 1,
+    location: 1,
+    timestamp: "2023-01-04T16:54:47-05:00",
+  },
+  {
+    flyerSize: 3,
+    campaign: 1,
+    designId: 1,
+    location: 1,
+    timestamp: "2023-01-04T16:42:47-05:00",
+  },
+  {
+    flyerSize: 3,
+    campaign: 1,
+    designId: 1,
+    location: 1,
+    timestamp: "2023-02-11T00:11:25-05:00",
+  },
+];

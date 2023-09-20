9e90ed2e7bc67931fa2a0a37f6157845c129ef73
Kenton Duprey <kentonduprey@icloud.com>
2023-09-20 03:25:47 -0400

    Updating imports and variable names to meet linting requirements

--- apps/site/src/data/calendlyResponses/getEventResponse.ts
+++ apps/site/src/data/calendlyResponses/getEventResponse.ts
@@ -1,275 +1,275 @@
-import { CalendlyEvent } from "@bpvs/types";
+import type { CalendlyEvent } from "@bpvs/types";
 
 export const getEventResponse: CalendlyEvent = {
-  resource: {
-    calendar_event: {
-      external_id: "29rbdjmvasfe8vg19o15onc1lc",
-      kind: "google",
-    },
-    created_at: "2022-09-17T20:08:21.017672Z",
-    end_time: "2022-09-25T20:00:00.000000Z",
-    event_guests: [],
-    event_memberships: [
-      {
-        user: "https://api.calendly.com/users/b5dadba5-411b-477e-b080-7b06e7072ee4",
-      },
-    ],
-    event_type:
-      "https://api.calendly.com/event_types/71e3dfdc-0750-4bb7-806a-60b8862df055",
-    invitees_counter: {
-      active: 1,
-      limit: 1,
-      total: 1,
-    },
-    location: {
-      data: {
-        id: 88276591767,
-        settings: {
-          global_dial_in_numbers: [
-            {
-              country_name: "US",
-              city: "New York",
-              number: "+1 6465588656",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              number: "+1 6469313860",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              city: "Washington DC",
-              number: "+1 3017158592",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              number: "+1 3092053325",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              city: "Chicago",
-              number: "+1 3126266799",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              city: "Tacoma",
-              number: "+1 2532158782",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              city: "Houston",
-              number: "+1 3462487799",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              number: "+1 3863475053",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              number: "+1 5642172000",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              number: "+1 6694449171",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              number: "+1 7193594580",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              city: "Denver",
-              number: "+1 7207072699",
-              type: "toll",
-              country: "US",
-            },
-          ],
-        },
-        extra: {
-          intl_numbers_url: "https://us06web.zoom.us/u/kmAbAQNfd",
-        },
-      },
-      join_url: "https://us06web.zoom.us/j/88276591767",
-      status: "pushed",
-      type: "zoom",
-    },
-    name: "Voice Lesson - Virtual",
-    start_time: "2022-09-25T19:00:00.000000Z",
-    status: "active",
-    updated_at: "2022-09-17T20:08:23.093748Z",
-    uri: "https://api.calendly.com/scheduled_events/fake",
-  },
+	resource: {
+		calendar_event: {
+			external_id: "29rbdjmvasfe8vg19o15onc1lc",
+			kind: "google",
+		},
+		created_at: "2022-09-17T20:08:21.017672Z",
+		end_time: "2022-09-25T20:00:00.000000Z",
+		event_guests: [],
+		event_memberships: [
+			{
+				user: "https://api.calendly.com/users/b5dadba5-411b-477e-b080-7b06e7072ee4",
+			},
+		],
+		event_type:
+			"https://api.calendly.com/event_types/71e3dfdc-0750-4bb7-806a-60b8862df055",
+		invitees_counter: {
+			active: 1,
+			limit: 1,
+			total: 1,
+		},
+		location: {
+			data: {
+				id: 88276591767,
+				settings: {
+					global_dial_in_numbers: [
+						{
+							country_name: "US",
+							city: "New York",
+							number: "+1 6465588656",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							number: "+1 6469313860",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							city: "Washington DC",
+							number: "+1 3017158592",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							number: "+1 3092053325",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							city: "Chicago",
+							number: "+1 3126266799",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							city: "Tacoma",
+							number: "+1 2532158782",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							city: "Houston",
+							number: "+1 3462487799",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							number: "+1 3863475053",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							number: "+1 5642172000",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							number: "+1 6694449171",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							number: "+1 7193594580",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							city: "Denver",
+							number: "+1 7207072699",
+							type: "toll",
+							country: "US",
+						},
+					],
+				},
+				extra: {
+					intl_numbers_url: "https://us06web.zoom.us/u/kmAbAQNfd",
+				},
+			},
+			join_url: "https://us06web.zoom.us/j/88276591767",
+			status: "pushed",
+			type: "zoom",
+		},
+		name: "Voice Lesson - Virtual",
+		start_time: "2022-09-25T19:00:00.000000Z",
+		status: "active",
+		updated_at: "2022-09-17T20:08:23.093748Z",
+		uri: "https://api.calendly.com/scheduled_events/fake",
+	},
 };
 
 export const consultationResponse: CalendlyEvent = {
-  resource: {
-    calendar_event: {
-      external_id: "99eip9o39cv01hb7i89lvf7v18",
-      kind: "google",
-    },
-    created_at: "2022-09-25T19:00:00.000000Z",
-    end_time: "2022-09-25T20:00:00.000000Z",
-    event_guests: [],
-    event_memberships: [
-      {
-        user: "https://api.calendly.com/users/b5dadba5-411b-477e-b080-7b06e7072ee4",
-      },
-    ],
-    event_type:
-      "https://api.calendly.com/event_types/7b5119f7-3cde-4746-8115-feaf467336df",
-    invitees_counter: {
-      active: 1,
-      limit: 1,
-      total: 1,
-    },
-    location: {
-      data: {
-        id: 85618732050,
-        settings: {
-          global_dial_in_numbers: [
-            {
-              country_name: "US",
-              city: "Washington DC",
-              number: "+1 3017158592",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              number: "+1 3052241968",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              number: "+1 3092053325",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              city: "Chicago",
-              number: "+1 3126266799",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              city: "New York",
-              number: "+1 6465588656",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              number: "+1 6469313860",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              number: "+1 5642172000",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              number: "+1 6694449171",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              number: "+1 6892781000",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              number: "+1 7193594580",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              city: "Denver",
-              number: "+1 7207072699",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              number: "+1 2532050468",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              city: "Tacoma",
-              number: "+1 2532158782",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              city: "Houston",
-              number: "+1 3462487799",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              number: "+1 3602095623",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              number: "+1 3863475053",
-              type: "toll",
-              country: "US",
-            },
-            {
-              country_name: "US",
-              number: "+1 5074734847",
-              type: "toll",
-              country: "US",
-            },
-          ],
-        },
-        extra: {
-          intl_numbers_url: "https://us06web.zoom.us/u/kcJwuMPnb",
-        },
-      },
-      join_url: "https://us06web.zoom.us/j/85618732050",
-      status: "pushed",
-      type: "zoom",
-    },
-    name: "Consultation Session",
-    start_time: "2022-09-25T19:00:00.000000Z",
-    status: "active",
-    updated_at: "2022-09-25T19:00:00.000000Z",
-    uri: "https://api.calendly.com/scheduled_events/321a9816-c09e-4dcc-b49e-c71b6f407690",
-  },
+	resource: {
+		calendar_event: {
+			external_id: "99eip9o39cv01hb7i89lvf7v18",
+			kind: "google",
+		},
+		created_at: "2022-09-25T19:00:00.000000Z",
+		end_time: "2022-09-25T20:00:00.000000Z",
+		event_guests: [],
+		event_memberships: [
+			{
+				user: "https://api.calendly.com/users/b5dadba5-411b-477e-b080-7b06e7072ee4",
+			},
+		],
+		event_type:
+			"https://api.calendly.com/event_types/7b5119f7-3cde-4746-8115-feaf467336df",
+		invitees_counter: {
+			active: 1,
+			limit: 1,
+			total: 1,
+		},
+		location: {
+			data: {
+				id: 85618732050,
+				settings: {
+					global_dial_in_numbers: [
+						{
+							country_name: "US",
+							city: "Washington DC",
+							number: "+1 3017158592",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							number: "+1 3052241968",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							number: "+1 3092053325",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							city: "Chicago",
+							number: "+1 3126266799",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							city: "New York",
+							number: "+1 6465588656",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							number: "+1 6469313860",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							number: "+1 5642172000",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							number: "+1 6694449171",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							number: "+1 6892781000",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							number: "+1 7193594580",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							city: "Denver",
+							number: "+1 7207072699",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							number: "+1 2532050468",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							city: "Tacoma",
+							number: "+1 2532158782",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							city: "Houston",
+							number: "+1 3462487799",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							number: "+1 3602095623",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							number: "+1 3863475053",
+							type: "toll",
+							country: "US",
+						},
+						{
+							country_name: "US",
+							number: "+1 5074734847",
+							type: "toll",
+							country: "US",
+						},
+					],
+				},
+				extra: {
+					intl_numbers_url: "https://us06web.zoom.us/u/kcJwuMPnb",
+				},
+			},
+			join_url: "https://us06web.zoom.us/j/85618732050",
+			status: "pushed",
+			type: "zoom",
+		},
+		name: "Consultation Session",
+		start_time: "2022-09-25T19:00:00.000000Z",
+		status: "active",
+		updated_at: "2022-09-25T19:00:00.000000Z",
+		uri: "https://api.calendly.com/scheduled_events/321a9816-c09e-4dcc-b49e-c71b6f407690",
+	},
 };

bd4345c0a12e68a78c37d54fb6aebd145835a7f5
Kenton Duprey <kentonduprey@icloud.com>
2023-09-21 23:31:32 -0400

    Adding missing libs and updating npmrc

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -5,7 +5,7 @@ const { withSentryConfig } = require("@sentry/nextjs");
 const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
 
 const nextConfig = withAxiom({
-	transpilePackages: ["@bpvs/config", "@bpvs/types"],
+	transpilePackages: ["@bpvs/config", "@bpvs/types", "@bpvs/db"],
 	reactStrictMode: true,
 	webpack(config, { isServer }) {
 		if (isServer) {

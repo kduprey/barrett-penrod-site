4a6b58f43c58f7ee110013d942e92a24801d54e2
Kenton Duprey <kentonduprey@icloud.com>
2023-09-29 00:55:05 -0400

    Ignoring test dirs and fixing imports

--- apps/site/vitest.config.ts
+++ apps/site/vitest.config.ts
@@ -1,48 +1,50 @@
-import react from "@vitejs/plugin-react";
 import path from "node:path";
+import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call -- vite-tsconfig-paths
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  envDir: "../../.env",
-  test: {
-    // coverage: {
-    //   reporter: ["text", "json", "html", "lcov"],
-    //   exclude: [
-    //     "coverage/**",
-    //     "dist/**",
-    //     "packages/*/test{,s}/**",
-    //     "**/*.d.ts",
-    //     "cypress/**",
-    //     "test{,s}/**",
-    //     "test{,-*}.{js,cjs,mjs,ts,tsx,jsx}",
-    //     "**/*{.,-}test.{js,cjs,mjs,ts,tsx,jsx}",
-    //     "**/*{.,-}spec.{js,cjs,mjs,ts,tsx,jsx}",
-    //     "**/__tests__/**",
-    //     "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress}.config.*",
-    //     "**/.{eslint,mocha,prettier}rc.{js,cjs,yml}",
-    //   ],
-    //   include: [
-    //     "pages/api/*.ts",
-    //     "pages/api/**/*.ts",
-    //     "utils/**",
-    //     "utils/**/*.ts",
-    //   ],
-    // },
-    globals: true,
-    isolate: true,
-    threads: false,
-  },
-  resolve: {
-    alias: {
-      "@bpvs/config": path.resolve(__dirname, "../../packages/config"),
-      "@bpvs/site": path.resolve(__dirname, "apps/site/src/"),
-      "@bpvs/utils": path.resolve(__dirname, "../../packages/utils"),
-      "@bpvs/db": path.resolve(__dirname, "../../packages/db"),
-      "@bpvs/types": path.resolve(__dirname, "../../packages/types"),
-      "@bpvs/validation": path.resolve(__dirname, "../../packages/validation"),
-    },
-  },
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-call -- vite-tsconfig-paths
+	plugins: [react(), tsconfigPaths()],
+	envDir: "../../.env",
+	test: {
+		// coverage: {
+		//   reporter: ["text", "json", "html", "lcov"],
+		//   exclude: [
+		//     "coverage/**",
+		//     "dist/**",
+		//     "packages/*/test{,s}/**",
+		//     "**/*.d.ts",
+		//     "cypress/**",
+		//     "test{,s}/**",
+		//     "test{,-*}.{js,cjs,mjs,ts,tsx,jsx}",
+		//     "**/*{.,-}test.{js,cjs,mjs,ts,tsx,jsx}",
+		//     "**/*{.,-}spec.{js,cjs,mjs,ts,tsx,jsx}",
+		//     "**/__tests__/**",
+		//     "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress}.config.*",
+		//     "**/.{eslint,mocha,prettier}rc.{js,cjs,yml}",
+		//   ],
+		//   include: [
+		//     "pages/api/*.ts",
+		//     "pages/api/**/*.ts",
+		//     "utils/**",
+		//     "utils/**/*.ts",
+		//   ],
+		// },
+		globals: true,
+		isolate: true,
+		threads: false,
+	},
+	resolve: {
+		alias: {
+			"@bpvs/config": path.resolve(__dirname, "../../packages/config"),
+			"@bpvs/site": path.resolve(__dirname, "apps/site/src/"),
+			"@bpvs/utils": path.resolve(__dirname, "../../packages/utils"),
+			"@bpvs/db": path.resolve(__dirname, "../../packages/db"),
+			"@bpvs/types": path.resolve(__dirname, "../../packages/types"),
+			"@bpvs/validation": path.resolve(__dirname, "../../packages/validation"),
+		},
+	},
 });

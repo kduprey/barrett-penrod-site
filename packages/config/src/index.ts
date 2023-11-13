5ad0372bb0e5816a68b5be09fba42d0649f73b46
Kenton Duprey <kentonduprey@icloud.com>
2023-11-13 15:33:52 -0500

    Updating stripe api version in initialiser

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -8,7 +8,7 @@ export const stripe = new Stripe(
     ? `${process.env.STRIPE_SECRET_KEY}`
     : `${process.env.STRIPE_TEST_SECRET_KEY}`,
   {
-    apiVersion: "2023-08-16",
+    apiVersion: "2023-10-16",
     typescript: true,
     appInfo: {
       name: "Booking Site",

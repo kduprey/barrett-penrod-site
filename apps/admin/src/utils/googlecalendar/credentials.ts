c5a57bee884a862fd27b10f60c706c4cbe7cdbf4
Kenton Duprey <kentonduprey@icloud.com>
2023-11-16 02:08:31 -0500

    Commenting out credentials lib file

--- apps/admin/src/utils/googlecalendar/credentials.ts
+++ apps/admin/src/utils/googlecalendar/credentials.ts
@@ -1,52 +1,52 @@
-import { prisma } from "@bpvs/db";
+// import { prisma } from "@bpvs/db";
 
-export const loadGCalCredentialsIfExist = async (authId: string) => {
-	const credentials = await prisma.calendar_credential.findUnique({
-		where: {
-			user_id: authId,
-		},
-	});
+// export const loadGCalCredentialsIfExist = async (authId: string) => {
+// 	const credentials = await prisma.calendar_credential.findUnique({
+// 		where: {
+// 			user_id: authId,
+// 		},
+// 	});
 
-	if (credentials && credentials.expiresAt.getTime() > Date.now()) {
-		return credentials;
-	}
-	return null;
-};
+// 	if (credentials && credentials.expiresAt.getTime() > Date.now()) {
+// 		return credentials;
+// 	}
+// 	return null;
+// };
 
-export const saveGCalCredentials = async (
-	authId: string,
-	credentials: {
-		scope: string;
-		token_type: "Bearer";
-		expiry_date: number;
-		access_token: string;
-		refresh_token: string;
-	}
-) => {
-	await prisma.calendar_credential.upsert({
-		where: {
-			user_id: authId,
-		},
-		create: {
-			user_id: authId,
-			name: "Google Calendar",
-			value: JSON.stringify(credentials),
-			expiresAt: new Date(Date.now() + credentials.expiry_date),
-		},
-		update: {
-			value: JSON.stringify(credentials),
-			expiresAt: new Date(Date.now() + credentials.expiry_date),
-		},
-	});
-};
+// export const saveGCalCredentials = async (
+// 	authId: string,
+// 	credentials: {
+// 		scope: string;
+// 		token_type: "Bearer";
+// 		expiry_date: number;
+// 		access_token: string;
+// 		refresh_token: string;
+// 	}
+// ) => {
+// 	await prisma.calendar_credential.upsert({
+// 		where: {
+// 			user_id: authId,
+// 		},
+// 		create: {
+// 			user_id: authId,
+// 			name: "Google Calendar",
+// 			value: JSON.stringify(credentials),
+// 			expiresAt: new Date(Date.now() + credentials.expiry_date),
+// 		},
+// 		update: {
+// 			value: JSON.stringify(credentials),
+// 			expiresAt: new Date(Date.now() + credentials.expiry_date),
+// 		},
+// 	});
+// };
 
-export const authoriseGCalCredentials = async (
-	authId: string,
-	credentials: {
-		scope: string;
-		token_type: "Bearer";
-		expiry_date: number;
-		access_token: string;
-		refresh_token: string;
-	}
-) => {};
+// export const authoriseGCalCredentials = async (
+// 	authId: string,
+// 	credentials: {
+// 		scope: string;
+// 		token_type: "Bearer";
+// 		expiry_date: number;
+// 		access_token: string;
+// 		refresh_token: string;
+// 	}
+// ) => {};

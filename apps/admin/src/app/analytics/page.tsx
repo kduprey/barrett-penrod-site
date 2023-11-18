f6374db9a125902b86e246ae32f4d85622f971da
Kenton Duprey <kentonduprey@icloud.com>
2023-11-18 00:30:10 -0500

    Adjusting analytics page layout for both tables

--- apps/admin/src/app/analytics/page.tsx
+++ apps/admin/src/app/analytics/page.tsx
@@ -1,26 +1,43 @@
 import { prisma } from "@bpvs/db";
-import { QrScansTable } from "@/components";
+import { Container, Tabs, TabsList, TabsPanel, TabsTab } from "@mantine/core";
+import { ContactSubmissionsTable, QrScansTable } from "@/components";
 
 const page = async () => {
-	const data = await prisma.qrScan.findMany();
+	const scanData = await prisma.qrScan.findMany();
 	const locations = await prisma.qrScanLocation.findMany();
 	const designs = await prisma.qrScanDesign.findMany();
 	const campaigns = await prisma.qrScanCampaign.findMany();
 	const flyerSizes = await prisma.qrScanFlyerSize.findMany();
-
-	const tableData = {
-		scanData: data,
-		locations,
-		designs,
-		campaigns,
-		flyerSizes,
-	};
+	const contactSubmissions = await prisma.contactSubmission.findMany();
 
 	return (
 		<>
 			<h1>Analytics</h1>
 			<div>
-				<QrScansTable {...tableData} />
+				<Tabs defaultValue="qrScans">
+					<TabsList>
+						<TabsTab value="qrScans">QR Code Scans</TabsTab>
+						<TabsTab value="contactSubmissions">Contact Submissions</TabsTab>
+					</TabsList>
+
+					<TabsPanel value="qrScans">
+						<Container fluid>
+							<h2>QR Code Scans</h2>
+							<QrScansTable
+								{...{ scanData, locations, designs, campaigns, flyerSizes }}
+							/>
+						</Container>
+					</TabsPanel>
+
+					<TabsPanel value="contactSubmissions">
+						<Container fluid>
+							<h2>Contact Submissions</h2>
+							<ContactSubmissionsTable
+								{...{ submissionData: contactSubmissions }}
+							/>
+						</Container>
+					</TabsPanel>
+				</Tabs>
 			</div>
 		</>
 	);

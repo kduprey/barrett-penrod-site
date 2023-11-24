2a7326bfd0056b51711f9a52087e27f1e269789e
Kenton Duprey <kentonduprey@icloud.com>
2023-11-24 02:56:57 -0500

    Removing component from barrel export

--- apps/admin/src/components/index.ts
+++ apps/admin/src/components/index.ts
@@ -4,4 +4,3 @@ export * from "./ColorSchemeToggle/ColorSchemeToggle";
 export * from "./Navbar/Navbar";
 export * from "./PageWrapper/PageWrapper";
 export * from "./Welcome/Welcome";
-export * from "./Widgets/GCalAuthWidget/GCalAuthWidget";

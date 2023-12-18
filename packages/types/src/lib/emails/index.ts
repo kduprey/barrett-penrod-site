4cbe4e1651cdbad94b7e0dffac85f33dcf3fb08d
Kenton Duprey <kentonduprey@icloud.com>
2023-12-18 13:10:50 -0500

    Adding formatBookingDate util for email date and time formating

--- packages/types/src/lib/emails/index.ts
+++ packages/types/src/lib/emails/index.ts
@@ -1,44 +1,49 @@
 import { PackageType, SessionLocation, SessionType } from "../events";
 
+export interface FormattedBookingDate {
+  bookingTime: string;
+  bookingDate: string;
+}
+
 export interface Contact {
-	email: string;
-	name?: string;
+  email: string;
+  name?: string;
 }
 
 export interface PackageEmailTemplateData {
-	packageName: string;
-	bookingDate: string;
-	bookingTime: string;
-	bookingLocation: string;
-	zoomLink?: string;
+  packageName: string;
+  bookingDate: string;
+  bookingTime: string;
+  bookingLocation: string;
+  zoomLink?: string;
 }
 
 export interface EmailTemplateData {
-	bookingDate: string;
-	bookingTime: string;
-	bookingName: string;
-	zoomLink?: string;
+  bookingDate: string;
+  bookingTime: string;
+  bookingName: string;
+  zoomLink?: string;
 }
 
 export interface EmailData {
-	client: Required<Contact>;
-	sessionType: SessionType;
-	bookingDate: Date;
-	bookingLocation: SessionLocation;
-	zoomLink?: string;
+  client: Required<Contact>;
+  sessionType: SessionType;
+  formattedBookingDate: FormattedBookingDate;
+  bookingLocation: SessionLocation;
+  zoomLink?: string;
 }
 
 export type ConsultationEmail = Omit<
-	Required<EmailData>,
-	"bookingLocation" | "sessionType"
+  Required<EmailData>,
+  "bookingLocation" | "sessionType"
 >;
 
 export interface GuestEmails extends Omit<EmailData, "client"> {
-	guests: Contact[];
+  guests: Contact[];
 }
 
 export interface PackageConfirmationEmail extends EmailData {
-	packageName: PackageType;
+  packageName: PackageType;
 }
 
 export type FirstTimeEmail = EmailData;

2c9e4cce76de35585a3d7ce89c27c8675b3928f0
Kenton Duprey <kentonduprey@icloud.com>
2023-12-18 13:11:41 -0500

    Updating email function dynamic data with updated date object

--- packages/emails-temp/src/lib/guestEmail.ts
+++ packages/emails-temp/src/lib/guestEmail.ts
@@ -5,70 +5,61 @@ import { sendgrid } from "./sendgrid";
 /**
  * This endpoint is used to send a guest email to a list of guests whom are attending a session.
  * @param guests - The guests to send the email to
- * @param bookingDate - The time of the booking
+ * @param formattedBookingDate - The formatted date and time of the booking
  * @param bookingName - The name of the booking
  * @param zoomLink - The zoom link for the booking
  * @returns The response from SendGrid
  */
 
 export const sendGuestEmails = async ({
-	guests,
-	sessionType,
-	bookingLocation,
-	bookingDate,
-	zoomLink,
+  guests,
+  sessionType,
+  bookingLocation,
+  formattedBookingDate,
+  zoomLink,
 }: GuestEmails): Promise<ClientResponse> => {
-	const templateId = "d-b628680e34354157b553625c036e2836";
+  const templateId = "d-b628680e34354157b553625c036e2836";
 
-	const message: MailDataRequired = {
-		from: {
-			email: "barrett@barrettpenrod.com",
-			name: "Barrett Penrod Voice Studio",
-		},
-		replyTo: {
-			email: "barrettpenrod@gmail.com",
-			name: "Barrett Penrod",
-		},
-		personalizations: [
-			{
-				to: [...guests],
-				dynamicTemplateData: {
-					bookingTime: new Date(bookingDate).toLocaleTimeString([], {
-						hour: "2-digit",
-						minute: "2-digit",
-					}),
-					bookingDate: new Date(bookingDate).toLocaleDateString([], {
-						weekday: "long",
-						month: "short",
-						day: "numeric",
-						year: "numeric",
-					}),
-					sessionType,
-					bookingLocation,
-					zoomLink,
-				},
-			},
-		],
-		templateId,
-		mailSettings: {
-			sandboxMode: {
-				enable: dev,
-			},
-		},
-	};
+  const message: MailDataRequired = {
+    from: {
+      email: "barrett@barrettpenrod.com",
+      name: "Barrett Penrod Voice Studio",
+    },
+    replyTo: {
+      email: "barrettpenrod@gmail.com",
+      name: "Barrett Penrod",
+    },
+    personalizations: [
+      {
+        to: [...guests],
+        dynamicTemplateData: {
+          ...formattedBookingDate,
+          sessionType,
+          bookingLocation,
+          zoomLink,
+        },
+      },
+    ],
+    templateId,
+    mailSettings: {
+      sandboxMode: {
+        enable: dev,
+      },
+    },
+  };
 
-	try {
-		console.log("Sending guest emails...");
+  try {
+    console.log("Sending guest emails...");
 
-		const response = await sendgrid.send(message);
-		console.log("Guest emails sent!");
+    const response = await sendgrid.send(message);
+    console.log("Guest emails sent!");
 
-		console.log(response);
-		return response[0];
-	} catch (error: unknown) {
-		console.error(error);
-		if (error instanceof Error)
-			throw new Error(`Error sending email: ${error.message}`);
-		throw new Error("Error sending email");
-	}
+    console.log(response);
+    return response[0];
+  } catch (error: unknown) {
+    console.error(error);
+    if (error instanceof Error)
+      throw new Error(`Error sending email: ${error.message}`);
+    throw new Error("Error sending email");
+  }
 };
--- packages/emails-temp/src/lib/singleBooking.ts
+++ packages/emails-temp/src/lib/singleBooking.ts
@@ -6,70 +6,61 @@ import { sendgrid } from "./sendgrid";
  * This endpoint is used to send a single booking email to a client after booking a session.
  * @param client - The email address and name of the client
  * @param sessionType - The type of session
- * @param bookingDate - The date of the event
+ * @param formattedBookingDate - The formatted date and time of the event
  * @param bookingLocation - The Location of the booking
  * @param zoomLink - The zoom link for the event (optional)
  * @returns - The response from SendGrid
  */
 
 export const sendSingleBookingEmail = async ({
-	client,
-	sessionType,
-	bookingDate,
-	bookingLocation,
-	zoomLink,
+  client,
+  sessionType,
+  formattedBookingDate,
+  bookingLocation,
+  zoomLink,
 }: SingleEmail): Promise<ClientResponse> => {
-	const templateId = "d-d3b1109a7bd44612b10c3e60ed9024da";
+  const templateId = "d-d3b1109a7bd44612b10c3e60ed9024da";
 
-	const message: MailDataRequired = {
-		from: {
-			email: "barrett@barrettpenrod.com",
-			name: "Barrett Penrod Voice Studio",
-		},
-		replyTo: {
-			email: "barrettpenrod@gmail.com",
-			name: "Barrett Penrod",
-		},
-		personalizations: [
-			{
-				to: {
-					...client,
-				},
-				dynamicTemplateData: {
-					sessionType,
-					bookingTime: new Date(bookingDate).toLocaleTimeString([], {
-						hour: "2-digit",
-						minute: "2-digit",
-					}),
-					bookingDate: new Date(bookingDate).toLocaleDateString([], {
-						weekday: "long",
-						month: "short",
-						day: "numeric",
-						year: "numeric",
-					}),
-					bookingLocation,
-					zoomLink,
-				},
-			},
-		],
-		templateId,
-		mailSettings: {
-			sandboxMode: {
-				enable: dev,
-			},
-		},
-	};
+  const message: MailDataRequired = {
+    from: {
+      email: "barrett@barrettpenrod.com",
+      name: "Barrett Penrod Voice Studio",
+    },
+    replyTo: {
+      email: "barrettpenrod@gmail.com",
+      name: "Barrett Penrod",
+    },
+    personalizations: [
+      {
+        to: {
+          ...client,
+        },
+        dynamicTemplateData: {
+          sessionType,
+          ...formattedBookingDate,
+          bookingLocation,
+          zoomLink,
+        },
+      },
+    ],
+    templateId,
+    mailSettings: {
+      sandboxMode: {
+        enable: dev,
+      },
+    },
+  };
 
-	try {
-		console.log("Sending single booking email...");
+  try {
+    console.log("Sending single booking email...");
 
-		const response = await sendgrid.send(message);
-		return response[0];
-	} catch (error: unknown) {
-		console.log(error);
-		if (error instanceof Error)
-			throw new Error(`Error sending email: ${error.message}`);
+    const response = await sendgrid.send(message);
+    return response[0];
+  } catch (error: unknown) {
+    console.log(error);
+    if (error instanceof Error)
+      throw new Error(`Error sending email: ${error.message}`);
 
-		throw new Error("Error sending email");
-	}
+    throw new Error("Error sending email");
+  }
 };

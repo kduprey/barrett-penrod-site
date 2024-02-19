20111127dbd2d43bb4529d571b7633f20db3bfc6
Kenton Duprey <kentonduprey@icloud.com>
2024-02-19 00:31:36 -0500

    Fixing stripe API key logic to only rely on env variable

--- packages/config/src/lib/stripe.ts
+++ packages/config/src/lib/stripe.ts
@@ -1,16 +1,10 @@
-import { dev } from "@bpvs/types";
 import Stripe from "stripe";
 
-export const stripe = new Stripe(
-	!dev
-		? `${process.env.STRIPE_SECRET_KEY}`
-		: `${process.env.STRIPE_TEST_SECRET_KEY}`,
-	{
-		apiVersion: "2023-10-16",
-		typescript: true,
-		appInfo: {
-			name: "Booking Site",
-			url: "https://barrettpendrod.com/bookings",
-		},
-	}
-);
+export const stripe = new Stripe(`${process.env.STRIPE_SECRET_KEY}`, {
+	apiVersion: "2023-10-16",
+	typescript: true,
+	appInfo: {
+		name: "Booking Site",
+		url: "https://barrettpendrod.com/bookings",
+	},
+});

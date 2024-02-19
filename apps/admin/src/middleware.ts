4993101d3c40b678eda608ad7d68148664a0468c
Kenton Duprey <kentonduprey@icloud.com>
2024-02-19 00:41:29 -0500

    Updating public routes in middleware

--- apps/admin/src/middleware.ts
+++ apps/admin/src/middleware.ts
@@ -2,7 +2,11 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
 	publicRoutes: ["/", "/login", "/sign-up"],
-	ignoredRoutes: ["/api/oauth/google/callback", "/api/oauth/zoom/callback"],
+	ignoredRoutes: [
+		"/api/oauth/google/callback",
+		"/api/oauth/zoom/callback",
+		"/api/oauth/stripe/callback",
+	],
 });
 
 export const config = {

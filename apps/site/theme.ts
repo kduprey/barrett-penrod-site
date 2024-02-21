136345ca61d6b96375736f8c3ed76cfd74288e21
Kenton Duprey <kentonduprey@icloud.com>
2024-02-20 23:33:11 -0500

    Adding mantine libs and base config and theme

---
+++ apps/site/theme.ts
@@ -0,0 +1,61 @@
+"use client";
+
+import { createTheme } from "@mantine/core";
+
+export const theme = createTheme({
+	fontFamily: "Montserrat, Arial, sans-serif",
+	headings: {
+		fontFamily: "Montserrat, Arial, sans-serif",
+	},
+	primaryColor: "primary",
+	colors: {
+		primary: [
+			"#e5ffff",
+			"#d4fbfa",
+			"#a8f6f3",
+			"#7af1ed",
+			"#58ede7",
+			"#45ebe4",
+			"#38e9e2",
+			"#28cfc9",
+			"#11b9b3",
+			"#00a09b",
+		],
+		secondary: [
+			"#fff1e4",
+			"#fde1d3",
+			"#f3c2a7",
+			"#eba179",
+			"#e48451",
+			"#e07237",
+			"#df6929",
+			"#c6591d",
+			"#b14e16",
+			"#9b410e",
+		],
+		blue: [
+			"#e5f4ff",
+			"#cfe4ff",
+			"#9fc6fc",
+			"#6ba6f8",
+			"#408bf5",
+			"#267af4",
+			"#1471f4",
+			"#0160da",
+			"#0055c4",
+			"#0049ae",
+		],
+		red: [
+			"#faeff1",
+			"#eedddf",
+			"#e0b6bb",
+			"#d38d96",
+			"#c86b75",
+			"#c15561",
+			"#bf4957",
+			"#a93b47",
+			"#97333e",
+			"#852935",
+		],
+	},
+});

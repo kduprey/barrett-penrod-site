e10e53858b6d4923836626078b5927bcc3945d8a
Kenton Duprey <kentonduprey@icloud.com>
2024-02-28 00:14:15 -0500

    Adding headshot carousel

--- apps/site/src/app/page.tsx
+++ apps/site/src/app/page.tsx
@@ -1,51 +1,86 @@
+"use client";
+
+import { Carousel, CarouselSlide } from "@mantine/carousel";
+import Autoplay from "embla-carousel-autoplay";
 import Image from "next/image";
-import headshot5 from "../../public/headshot5.jpg";
-import {
-	About,
-	Footer,
-	Headline,
-	Headshots,
-	News,
-	PageBar,
-	Photos,
-	ResumeTable,
-	Showreel,
-	SocialBar,
-} from "@/components";
+import { useRef } from "react";
+import headshot1 from "public/headshot1.jpg";
+import headshot2 from "public/headshot2.jpg";
+import headshot3 from "public/headshot3.jpg";
+import headshot4 from "public/headshot4.jpg";
+import headshot5 from "public/headshot5.jpg";
+import headshot6 from "public/headshot6.jpg";
 
-const Page = () => (
-	<>
-		<PageBar />
+const images = [
+	headshot1,
+	headshot2,
+	headshot3,
+	headshot4,
+	headshot5,
+	headshot6,
+];
 
-		<SocialBar />
-		<section id="home">
-			{/* Heading Text */}
-			<Headline />
+const Page = () => {
+	const autoplay = useRef(Autoplay({ delay: 3000 }));
 
-			{/* Headshot 1 */}
-			<div className="flex w-full flex-col items-center justify-center bg-slate-400 py-4 md:hidden">
+	const slides = images.map((image, index) => (
+		<CarouselSlide key={index}>
+			<Image
+				alt={`Image ${index}`}
+				height={0}
+				sizes="100vw"
+				src={image}
+				style={{ objectFit: "cover", height: "100%", width: "100%" }}
+				width={0}
+			/>
+		</CarouselSlide>
+	));
+
+	return (
+		<>
+			{/* <PageBar /> */}
+
+			{/* <SocialBar /> */}
+			<section id="home">
+				<Carousel
+					height="75%"
+					loop
+					plugins={[autoplay.current]}
+					slideSize="100%"
+					w="40%"
+					withControls={false}
+					withIndicators
+				>
+					{slides}
+				</Carousel>
+
+				{/* Heading Text */}
+				{/* <Headline /> */}
+
+				{/* Headshot 1 */}
+				{/* <div className="flex w-full flex-col items-center justify-center bg-slate-400 py-4 md:hidden">
 				<Image
 					alt="Barret Penrod"
 					loading="lazy"
 					placeholder="blur"
 					src={headshot5}
 				/>
-			</div>
+			</div> */}
 
-			{/* About Section */}
+				{/* About Section */}
 
-			<About />
+				{/* <About /> */}
 
-			{/* Resume/Headshots */}
+				{/* Resume/Headshots */}
 
-			<Headshots />
+				{/* <Headshots />
 
 			<div className="flex justify-center p-3">
 				<hr className="h-1 w-3/4" />
-			</div>
+			</div> */}
 
-			{/* Resume */}
-			<div className="py-6 md:p-0" id="resume">
+				{/* Resume */}
+				{/* <div className="py-6 md:p-0" id="resume">
 				<h1 className="text-center text-secondary">Resumé</h1>
 
 				<ResumeTable />
@@ -59,22 +94,23 @@ const Page = () => (
 						Download Resume
 					</a>
 				</div>
-			</div>
+			</div> */}
 
-			{/* Showreel */}
+				{/* Showreel */}
 
-			<Showreel />
+				{/* <Showreel /> */}
 
-			{/* Photos */}
+				{/* Photos */}
 
-			<Photos />
+				{/* <Photos /> */}
 
-			{/* News */}
+				{/* News */}
 
-			<News />
-		</section>
-		<Footer />
-	</>
-);
+				{/* <News /> */}
+			</section>
+			{/* <Footer /> */}
+		</>
+	);
+};
 
 export default Page;

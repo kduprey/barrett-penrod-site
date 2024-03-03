60194eb72104831420e5539c025c774d052066dc
Kenton Duprey <kentonduprey@icloud.com>
2024-03-02 21:33:59 -0500

    Cleaning up eslint config

--- apps/site/.eslintrc.js
+++ apps/site/.eslintrc.js
@@ -1,15 +1,11 @@
+// @ts-check
+/** @type {import("eslint").Linter.Config} */
+
 module.exports = {
 	root: true,
-	extends: ["@bpvs/eslint-config/eslint/next"].map(require.resolve),
+	extends: ["@bpvs/eslint-config/eslint/next"].map((v) => require.resolve(v)),
 	parserOptions: {
+		project: true,
 		tsconfigRootDir: __dirname,
-		project: ["./tsconfig.json"],
-	},
-	settings: {
-		"import/resolver": {
-			typescript: {
-				project: ["./tsconfig.json"],
-			},
-		},
 	},
 };
--- packages/style-guide/.eslintrc.js
+++ packages/style-guide/.eslintrc.js
@@ -1,3 +1,5 @@
+// @ts-check
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
-	extends: ["@vercel/style-guide/eslint/node"].map(require.resolve),
+	extends: ["@vercel/style-guide/eslint/node"].map((v) => require.resolve(v)),
 };
